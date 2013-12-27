ee15e4c792843ebc5147c7d49797a25cbff05aa1
Jesse Vincent <jesse@bestpractical.com>
2013-12-27 14:46:24 -0500

    add F keys to qwerty maps

--- layouts-generated/qwerty_fn2.h
+++ layouts-generated/qwerty_fn2.h
@@ -1,4 +1,4 @@
-#define KEYMAP_QWERTY_FN2 { /* Generated keymap for QWERTY_FN2 */ 	{Key_skip, Key_1, Key_2, Key_3, Key_4, Key_5, Key_skip, Key_skip, Key_6, Key_7, Key_8, Key_9, Key_0, Key_skip},\
+#define KEYMAP_QWERTY_FN2 { /* Generated keymap for QWERTY_FN2 */ 	{Key_skip, Key_F1, Key_F2, Key_F3, Key_F4, Key_F5, Key_skip, Key_skip, Key_F6, Key_F7, Key_F8, Key_F9, Key_F10, Key_skip},\
 	{Key_Tab, Key_macroKey1, Key_mouseBtnL, Key_mouseBtnM, Key_mouseBtnR, Key_T, Key_NoKey, Key_NoKey, Key_LCurlyBracket, Key_RCurlyBracket, Key_LSquareBracket, Key_RSquareBracket, Key_P, Key_Equals},\
 	{Key_PageUp, Key_mouseL, Key_mouseUp, Key_mouseDn, Key_mouseR, Key_G, Key_Tab, Key_Return, Key_LArrow, Key_DnArrow, Key_UpArrow, Key_RArrow, Key_Semicolon, Key_Quote},\
 	{Key_PageDn, Key_Keymap2, Key_mouseBtnL, Key_mouseBtnM, Key_mouseBtnR, Key_B, Key_Esc, Key_Enter, Key_Pipe, Key_M, Key_Comma, Key_Period, Key_Backslash, Key_Minus},\
