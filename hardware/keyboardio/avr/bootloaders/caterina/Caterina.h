baf4bb23b6e56e50997224c3750eb905890afd01
Jesse Vincent <jesse@bestpractical.com>
2015-03-25 14:06:43 -0700

    In our bootloader, abuse one of the TX/RX LEDs (D5) as the boot status LED

--- hardware/keyboardio/avr/bootloaders/caterina/Caterina.h
+++ hardware/keyboardio/avr/bootloaders/caterina/Caterina.h
@@ -66,17 +66,17 @@
 		
 		#define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
 		#define LED_SETUP()		DDRC |= (1<<7); DDRB |= (1<<0); DDRD |= (1<<5);
-		#define L_LED_OFF()		PORTC &= ~(1<<7)
-		#define L_LED_ON()		PORTC |= (1<<7)
-		#define L_LED_TOGGLE()	PORTC ^= (1<<7)
+		#define TX_LED_OFF()		PORTC &= ~(1<<7)
+		#define TX_LED_ON()		PORTC |= (1<<7)
+		#define L_LED_TOGGLE()	PORTD ^= (1<<5)
 		#if DEVICE_PID == 0x0037	// polarity of the RX and TX LEDs is reversed on the Micro
 			#define TX_LED_OFF()	PORTD &= ~(1<<5)
 			#define TX_LED_ON()		PORTD |= (1<<5)
 			#define RX_LED_OFF()	PORTB &= ~(1<<0)
 			#define RX_LED_ON()		PORTB |= (1<<0)			
 		#else 
-			#define TX_LED_OFF()	PORTD |= (1<<5)
-			#define TX_LED_ON()		PORTD &= ~(1<<5)
+			#define L_LED_OFF()	PORTD |= (1<<5)
+			#define L_LED_ON()		PORTD &= ~(1<<5)
 			#define RX_LED_OFF()	PORTB |= (1<<0)
 			#define RX_LED_ON()		PORTB &= ~(1<<0)
 		#endif
