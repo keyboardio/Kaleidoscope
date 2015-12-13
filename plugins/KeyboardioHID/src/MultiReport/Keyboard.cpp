bf4671d7d1eb1550b535d95ce746455c90e5f4b3
Jesse Vincent <jesse@bestpractical.com>
2015-12-13 15:56:50 -0800

    Convert the NKRO Keyboard's descriptor to use my primitive library

--- plugins/KeyboardioHID/src/MultiReport/Keyboard.cpp
+++ plugins/KeyboardioHID/src/MultiReport/Keyboard.cpp
@@ -22,44 +22,45 @@ THE SOFTWARE.
 */
 
 #include "Keyboard.h"
+#include "DescriptorPrimitives.h"
 
 static const uint8_t _hidMultiReportDescriptorKeyboard[] PROGMEM = {
     //  NKRO Keyboard
-    0x05, 0x01,                      /* USAGE_PAGE (Generic Desktop)	  47 */
-    0x09, 0x06,                      /* USAGE (Keyboard) */
-    0xa1, 0x01,                      /* COLLECTION (Application) */
-    0x85, HID_REPORTID_NKRO_KEYBOARD,	 /*   REPORT_ID */
-    0x05, 0x07,                      /*   USAGE_PAGE (Keyboard) */
+    _USAGE_PAGE, _PAGE_GENERIC_DESKTOP,         /* USAGE_PAGE (Generic Desktop)	  47 */
+    _USAGE, _USAGE_KEYBOARD,                    /* USAGE (Keyboard) */
+    _COLLECTION, _APPLICATION,                  /* COLLECTION (Application) */
+    _REPORT_ID, HID_REPORTID_NKRO_KEYBOARD,	    /*   REPORT_ID */
+    _USAGE_PAGE, _PAGE_KEYBOARD,                /*   USAGE_PAGE (Keyboard) */
 
     /* Keyboard Modifiers (shift, alt, ...) */
-    0x19, 0xe0,                      /*   USAGE_MINIMUM (Keyboard LeftControl) */
-    0x29, 0xe7,                      /*   USAGE_MAXIMUM (Keyboard Right GUI) */
-    0x15, 0x00,                      /*   LOGICAL_MINIMUM (0) */
-    0x25, 0x01,                      /*   LOGICAL_MAXIMUM (1) */
-    0x75, 0x01,                      /*   REPORT_SIZE (1) */
-    0x95, 0x08,                      /*   REPORT_COUNT (8) */
-    0x81, 0x02,                      /*   INPUT (Data,Var,Abs) */
+    _USAGE_MINIMUM, HID_KEYBOARD_LEFT_CONTROL, /*   USAGE_MINIMUM (Keyboard LeftControl) */
+    _USAGE_MAXIMUM, HID_KEYBOARD_RIGHT_GUI,    /*   USAGE_MAXIMUM (Keyboard Right GUI) */
+    _LOGICAL_MINIMUM, 0x00,                    /*   LOGICAL_MINIMUM (0) */
+    _LOGICAL_MAXIMUM, 0x01,                    /*   LOGICAL_MAXIMUM (1) */
+    _REPORT_SIZE, 0x01,                        /*   REPORT_SIZE (1) */
+    _REPORT_COUNT, 0x08,                       /*   REPORT_COUNT (8) */
+    _INPUT, (_DATA|_VARIABLE|_ABSOLUTE),       /*   INPUT (Data,Var,Abs) */
 
     /* 104 Keys as bitmap */
-    0x19, 0x00,						/*   Usage Minimum (0) */
-    0x29, NKRO_KEY_COUNT - 1,		/*   Usage Maximum (103) */
-    0x15, 0x00,						/*   Logical Minimum (0) */
-    0x25, 0x01,						/*   Logical Maximum (1) */
-    0x75, 0x01,						/*   Report Size (1) */
-    0x95, NKRO_KEY_COUNT,			/*   Report Count (104) */
-    0x81, 0x02,						/*   Input (Data, Variable, Absolute) */
+    _USAGE_MINIMUM, HID_KEYBOARD_NO_EVENT,	/*   Usage Minimum (0) */
+    _USAGE_MAXIMUM, NKRO_KEY_COUNT - 1,		/*   Usage Maximum (103) */
+    _LOGICAL_MINIMUM, 0x00,					/*   Logical Minimum (0) */
+    _LOGICAL_MAXIMUM, 0x01,					/*   Logical Maximum (1) */
+    _REPORT_SIZE, 0x01,						/*   Report Size (1) */
+    _REPORT_COUNT, NKRO_KEY_COUNT,			/*   Report Count (104) */
+    _INPUT, (_DATA|_VARIABLE|_ABSOLUTE),	/*   Input (Data, Variable, Absolute) */
 
     /* 1 Custom Keyboard key */
-    0x95, 0x01,                      /*   REPORT_COUNT (1) */
-    0x75, 0x08,                      /*   REPORT_SIZE (8) */
-    0x15, 0x00,                      /*   LOGICAL_MINIMUM (0) */
-    0x26, 0xE7, 0x00,                /*   LOGICAL_MAXIMUM (231) */
-    0x19, 0x00,                      /*   USAGE_MINIMUM (Reserved (no event indicated)) */
-    0x29, 0xE7,                      /*   USAGE_MAXIMUM (Keyboard Right GUI) */
-    0x81, 0x00,                      /*   INPUT (Data,Ary,Abs) */
+    _REPORT_COUNT, 0x01,                                        /*   REPORT_COUNT (1) */
+    _REPORT_SIZE, 0x08,                                         /*   REPORT_SIZE (8) */
+    _LOGICAL_MINIMUM, HID_KEYBOARD_NO_EVENT,                    /*   LOGICAL_MINIMUM (0) */
+    _MULTIBYTE(_LOGICAL_MAXIMUM), HID_KEYBOARD_RIGHT_GUI, 0x00, /*   LOGICAL_MAXIMUM (231) */
+    _USAGE_MINIMUM, HID_KEYBOARD_NO_EVENT,                      /*   USAGE_MINIMUM (Reserved (no event indicated)) */
+    _USAGE_MAXIMUM, HID_KEYBOARD_RIGHT_GUI,    /*   USAGE_MAXIMUM (Keyboard Right GUI) */
+    _INPUT, (_DATA|_ARRAY|_ABSOLUTE),          /*   INPUT (Data,Ary,Abs) */
 
     /* End */
-    0xC0						     /*   End Collection */
+    _END_COLLECTION						     /*   End Collection */
 };
 
 Keyboard_::Keyboard_(void) {
