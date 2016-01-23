852ee13f7ba7bbc512be40d04648a3bad69cfecb
Jesse Vincent <jesse@bestpractical.com>
2016-01-22 19:55:49 -0800

    Remove now-outdated config for older hardware

--- KeyboardConfig.h
+++ KeyboardConfig.h
@@ -5,28 +5,9 @@
 
 #define EEPROM_KEYMAP_LOCATION 0
 
-#define MODEL01 true
-
-#ifdef SYMMETRIC60
-#define ROWS 5
-#define COLS 14
-static const byte colPins[COLS] = {
-    A0, 3,13,    5, 10,  9,  8,  6, 12, 4, 11, 1, 0, 2
-};
-
-static const byte rowPins[ROWS] = { A5,A4,A3,A2,A1};
-#define LED_DATA_PIN  7
-#define LED_COUNT 0
-static const int key_led_map[ROWS][COLS] = {};
-#endif
-
-#ifdef MODEL01
-
 
 int RIGHT_COLS=8;
 int RIGHT_ROWS=4;
-
-
 int LEFT_COLS=8;
 int LEFT_ROWS=4;
 int left_colpins[]= {7,6,5,4,3,2,1,0};
@@ -43,7 +24,3 @@ int right_rowpins[]= {8,9,10,11};
 #define KEYMAPS 3
 #define NUMPAD_KEYMAP 2
 #define KEYMAP_LIST KEYMAP_QWERTY KEYMAP_GENERIC_FN2 KEYMAP_NUMPAD
-
-
-#endif
-
