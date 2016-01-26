90301322ea8a32f37939633e2c6f290010225afc
Jesse Vincent <jesse@bestpractical.com>
2016-01-25 17:39:56 -0800

    migrate some mouse-wide var definitions and macros into header file

--- mouse_movement.cpp
+++ mouse_movement.cpp
@@ -17,23 +17,6 @@ void _warp_jump(long left, long top, long height, long width) {
 
 
 
-// apparently, the mac discards 15% of the value space for mouse movement.
-// need to test this on other platforms
-//
-
-#define HALF_WIDTH 16384
-#define HALF_HEIGHT 16384
-
-
-
-int abs_left = 0;
-int abs_top = 0;
-
-int next_width;
-int next_height;
-int section_top;
-int section_left;
-boolean is_warping = false;
 
 void begin_warping() {
     section_left = abs_left;
--- mouse_movement.h
+++ mouse_movement.h
@@ -1,6 +1,26 @@
 
 #pragma once
 
+
+// Warping
 double mouseActiveForCycles = 0;
 float carriedOverX = 0;
 float carriedOverY = 0;
+
+
+// apparently, the mac discards 15% of the value space for mouse movement.
+// need to test this on other platforms
+
+#define HALF_WIDTH 16384
+#define HALF_HEIGHT 16384
+
+
+
+int abs_left = 0;
+int abs_top = 0;
+
+int next_width;
+int next_height;
+int section_top;
+int section_left;
+boolean is_warping = false;
