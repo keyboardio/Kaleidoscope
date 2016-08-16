51eba859069fe2be850e9a1215a4df3d87bef69a
Scott Perry <github@numist.net>
2016-08-15 22:19:04 -0700

    Move firmware into /source, deps as submodules in /libraries

--- source/KeyboardConfig.cpp
+++ source/KeyboardConfig.cpp
@@ -6,7 +6,7 @@ HARDWARE_IMPLEMENTATION KeyboardHardware;
 void led_set_crgb_at(uint8_t i, cRGB crgb) {
     KeyboardHardware.led_set_crgb_at(i,crgb);
 }
-cRGB led_get_crgb_at(uint8_t i){ 
+cRGB led_get_crgb_at(uint8_t i) {
     KeyboardHardware.led_get_crgb_at(i);
 }
 void led_sync(void) {
