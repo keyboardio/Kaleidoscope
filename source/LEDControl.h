de92bcc0de1c252fd49ed10e28d21626c563a5a1
Jesse Vincent <jesse@bestpractical.com>
2016-09-01 16:50:26 -0700

    Speed up rainbow and rainbow wave effects

--- source/LEDControl.h
+++ source/LEDControl.h
@@ -49,8 +49,8 @@ class LEDControl_ {
     static const byte rainbow_saturation = 255;
     static const byte rainbow_value = 255;
 
-    static const long rainbow_wave_ticks = 10; //delays between update
-    static const long rainbow_ticks = 5; //delays between update
+    static const long rainbow_wave_ticks = 1; //delays between update
+    static const long rainbow_ticks = 0; //delays between update
     long rainbow_current_ticks=0;
 
     uint8_t breathe_brightness = 0;    // how bright the LED is
