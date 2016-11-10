a599d4601a7c65922a7e8a20d0a169de1536bea7
Gergely Nagy <algernon@madhouse-project.org>
2016-11-10 13:31:10 +0100

    Fix led_get_crgb_at
    The function should return a value.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- KeyboardConfig.cpp
+++ KeyboardConfig.cpp
@@ -7,7 +7,7 @@ void led_set_crgb_at(uint8_t i, cRGB crgb) {
     KeyboardHardware.led_set_crgb_at(i,crgb);
 }
 cRGB led_get_crgb_at(uint8_t i) {
-    KeyboardHardware.led_get_crgb_at(i);
+    return KeyboardHardware.led_get_crgb_at(i);
 }
 void led_sync(void) {
     KeyboardHardware.led_sync();
