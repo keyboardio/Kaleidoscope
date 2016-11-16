2734edb2b05a8783777887747f75d8370a9ab6a3
Gergely Nagy <algernon@madhouse-project.org>
2016-11-16 21:46:21 +0100

    hooks: Add a way to replace hooks
    To make replacing work sanely, we first NULL out both the eventHandlers
    and loopHooks arrays in the Keyboardio_ constructor. This allows the
    replace functions to just run through the whole array, and either see
    hook pointers there, or NULL. So they don't need to be afraid of garbage
    being there.
    
    This makes the replacing very easy: run through the array, and replace
    the first occurrence of the old hook with the new. This further
    simplifies addition: the old hook we pass in, will be NULL. If we run
    out of space, it silently fails, like before.
    
    Replacing hooks is important for cases where one wants to build features
    that can be toggled off, or their behaviour otherwise changed at
    run-time. In this case, the most efficent way is to replace the hook,
    which is what these new helpers allow us to do.
    
    This closes #36.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/hooks.h
+++ src/hooks.h
@@ -9,8 +9,10 @@ typedef bool (*custom_handler_t)(Key mappedKey, byte row, byte col, uint8_t curr
 extern custom_handler_t eventHandlers[HOOK_MAX];
 
 void event_handler_hook_add (custom_handler_t hook);
+void event_handler_hook_replace (custom_handler_t oldHook, custom_handler_t newHook);
 
 typedef void (*custom_loop_t)(void);
 extern custom_loop_t loopHooks[HOOK_MAX];
 
 void loop_hook_add (custom_loop_t hook);
+void loop_hook_replace (custom_loop_t oldHook, custom_loop_t newHook);
