8425071a766091fc9c8309b61d5e9d9a22148644
Gergely Nagy <algernon@madhouse-project.org>
2016-11-24 10:30:32 +0100

    Drop the single-aritiy keyswitch state helpers
    They are not used anywhere, and as such, are a distraction. The states
    will be reworked into something similar that was now dropped, but that's
    a separate patch.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/keyswitch_state.h
+++ src/keyswitch_state.h
@@ -5,11 +5,6 @@
 
 #define INJECTED B10000000
 
-boolean key_was_pressed (byte keyState);
-boolean key_is_pressed (byte keyState);
-boolean key_toggled_on(byte keyState);
-boolean key_toggled_off(byte keyState);
-
 boolean key_was_pressed (uint8_t currentState, uint8_t previousState);
 boolean key_is_pressed (uint8_t currentState, uint8_t previousState);
 boolean key_toggled_on(uint8_t currentState, uint8_t previousState);
