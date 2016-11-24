6a02fa8380751f788eddf7d5cdba525215cbcdad
Gergely Nagy <algernon@madhouse-project.org>
2016-11-24 10:30:32 +0100

    Drop the single-aritiy keyswitch state helpers
    They are not used anywhere, and as such, are a distraction. The states
    will be reworked into something similar that was now dropped, but that's
    a separate patch.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/keyswitch_state.cpp
+++ src/keyswitch_state.cpp
@@ -3,24 +3,8 @@
 
 #define PRESSED B00000001
 
-boolean key_was_pressed (byte keyState) {
-    if ( keyState & B00001000 || keyState & B00000100) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 boolean key_was_pressed (uint8_t currentState, uint8_t previousState) {
     if (previousState & PRESSED) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-boolean key_is_pressed (byte keyState) {
-    if ( keyState & B00000001 && keyState & B00000010 ) {
         return true;
     } else {
         return false;
@@ -35,25 +19,8 @@ boolean key_is_pressed (uint8_t currentState, uint8_t previousState) {
     }
 }
 
-boolean key_toggled_on(byte keyState) {
-    if (key_is_pressed(keyState) && ! key_was_pressed(keyState)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 boolean key_toggled_on (uint8_t currentState, uint8_t previousState) {
     if ((currentState & PRESSED) && ! (previousState & PRESSED) ) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
-boolean key_toggled_off(byte keyState) {
-    if (key_was_pressed(keyState) && ! key_is_pressed(keyState)) {
         return true;
     } else {
         return false;
