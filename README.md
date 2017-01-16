a722fea88d9dacaaae4932249f2cfc4e5ed59af8
Gergely Nagy <algernon@madhouse-project.org>
2017-01-16 15:18:13 +0100

    Initial import
    Lifted out of the monolithic Akela library.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ README.md
@@ -0,0 +1,60 @@
+# Akela-Heatmap
+
+The `Heatmap` plugin provides a LED effect, that displays a heatmap on the
+keyboard. The LEDs under each key will have a color according to how much use
+they see. Fewer used keys will have deep blue colors, that gradually turns
+lighter, then green, to yellow, and finally red for the most used keys. The
+heatmap is not updated on every key press, but periodically. It's precision is
+also an approximation, and not a hundred percent exact. Nevertheless, it is a
+reasonable estimate.
+
+## Using the plugin
+
+The plugin comes with reasonable defaults pre-configured, all one needs to do is
+include the header, and make sure the plugin is in use:
+
+```c++
+#include <Akela-Heatmap.h>
+
+void setup (void) {
+  Keyboardio.setup (KEYMAP_SIZE);
+  Keyboardio.use (&HeatmapEffect, NULL);
+}
+```
+
+This sets up the heatmap to update every 500 cycles, which is about 2.5 seconds,
+and is the default. It also registers a new LED effect, which means that if you
+have not set up any other effects, then Heatmap will likely be the default. You
+may not want that, so setting up at least one other LED effect, such as `LEDOff`
+is highly recommended.
+
+## Plugin methods
+
+The plugin provides two methods on the `HeatmapEffect` object:
+
+### `.configure(updateFrequency)`
+
+> Sets up the update frequency of the heatmap. The smaller this value is, the
+> more often the heatmap gets updated, but that comes with a cost: updating the
+> heatmap takes a fairly large amount of computation, and is not fast. Doing it
+> often will considerably slow down the keyboard, and that is rarely a desirable
+> thing.
+>
+> Nevertheless, the frequency preferred is a very subjective thing, which is why
+> this function exists. Give it a number, and it will wait that many scan cycles
+> between updates.
+>
+> Defaults to *500*.
+
+### `.activate()`
+
+> When called, immediately activates the Heatmap effect. Mostly useful in the
+> `setup()` method of the Sketch, or in macros that are meant to switch to the
+> heatmap effect, no matter where we are in the list.
+
+## Further reading
+
+Starting from the [example][plugin:example] is the recommended way of getting
+started with the plugin.
+
+ [plugin:example]: https://github.com/Akela-Plugins/Akela-Heatmap/blob/master/examples/Heatmap/Heatmap.ino
---
+++ src/Akela/Heatmap.cpp
@@ -0,0 +1,123 @@
+/* -*- mode: c++ -*-
+ * Akela -- Animated Keyboardio Extension Library for Anything
+ * Copyright (C) 2016, 2017  Gergely Nagy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Akela-Heatmap.h>
+
+namespace Akela {
+  static uint8_t heatmap[ROWS][COLS];
+  static uint16_t totalKeys;
+  static uint8_t highestCount;
+  static uint16_t updateFrequency = 500;
+  static uint16_t loopCount;
+
+  static const float heatColors[][3] = {{0.3, 0.3, 0.3}, {0.3, 1, 0.3}, {1, 1, 0.3}, {1, 0.3, 0.3}};
+
+  static void
+  shiftStats (void) {
+    highestCount = totalKeys = 0;
+    for (uint8_t r = 0; r < ROWS; r++) {
+      for (uint8_t c = 0; c < COLS; c++) {
+        heatmap[r][c] = heatmap[r][c] >> 1;
+        totalKeys += heatmap[r][c];
+        if (heatmap[r][c] > highestCount)
+          highestCount = heatmap[r][c];
+      }
+    }
+  }
+
+  static cRGB
+  computeColor (float v) {
+    float fb = 0;
+    uint8_t idx1, idx2;
+
+    if (v <= 0) {
+      idx1 = idx2 = 0;
+    } else if (v >= 1) {
+      idx1 = idx2 = 3;
+    } else {
+      float val = v * 3;
+      idx1 = int(floor(val));
+      idx2 = idx1 + 1;
+      fb = val - float(idx1);
+    }
+
+    uint8_t r = (int)(((heatColors[idx2][0] - heatColors[idx1][0]) * fb + heatColors[idx1][0]) * 255);
+    uint8_t g = (int)(((heatColors[idx2][1] - heatColors[idx1][1]) * fb + heatColors[idx1][1]) * 255);
+    uint8_t b = (int)(((heatColors[idx2][2] - heatColors[idx1][2]) * fb + heatColors[idx1][2]) * 255);
+
+    return {r, g, b};
+  }
+
+  Heatmap::Heatmap (void) {
+  }
+
+  void
+  Heatmap::begin (void) {
+    LEDControl.mode_add (this);
+    event_handler_hook_add (this->eventHook);
+    loop_hook_add (this->loopHook);
+  }
+
+  void
+  Heatmap::configure (uint16_t updateFreq) {
+    updateFrequency = updateFreq;
+  }
+
+  Key
+  Heatmap::eventHook (Key mappedKey, byte row, byte col, uint8_t keyState) {
+    // if it is a synthetic key, skip it.
+    if (keyState & INJECTED)
+      return mappedKey;
+
+    // if the key is not toggled on, return.
+    if (key_toggled_on (keyState))
+      return mappedKey;
+
+    totalKeys++;
+    heatmap[row][col]++;
+    if (heatmap[row][col] > highestCount)
+      highestCount = heatmap[row][col];
+
+    return mappedKey;
+  }
+
+  void
+  Heatmap::loopHook (bool postClear) {
+    if (highestCount > 191 || totalKeys > 16000)
+      shiftStats();
+  }
+
+  void
+  Heatmap::update (void) {
+    loopCount++;
+
+    if (loopCount < updateFrequency)
+      return;
+
+    loopCount = 0;
+    for (uint8_t r = 0; r < ROWS; r++) {
+      for (uint8_t c = 0; c < COLS; c++) {
+        uint8_t cap = totalKeys || 1;
+        float v = float(heatmap[r][c]) / cap;
+        led_set_crgb_at (r, c, computeColor (v));
+      }
+    }
+  }
+};
+
+Akela::Heatmap HeatmapEffect;
