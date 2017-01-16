b8088abbe129eb5e17d7bb55243ec2379e5d1b54
Gergely Nagy <algernon@madhouse-project.org>
2017-01-16 15:18:13 +0100

    Initial import
    Lifted out of the monolithic Akela library.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ README.md
@@ -0,0 +1,64 @@
+# Akela-LEDEffects
+
+The `LEDEffects` plugin provides a selection of LED effects, each of them fairly
+simple, simple enough to not need a plugin of their own. 
+
+## Using the plugin
+
+Because of containing a number of different effects, and not wanting to force
+all of them on the user, the plugin behaves differently than most others: it
+does not enable anything by default, and we will have to selectively enable the
+effects we are interested in.
+
+```c++
+#include <Akela-LEDEffects.h>
+```
+
+Then, in the `setup()` method of our Sketch, we will call `Keyboardio.use()`,
+with the selected effect objects.
+
+## Included effects
+
+All of these effects will scan the active layers, and apply effects based on
+what keys are active on each position, thus, it needs no hints or configuration
+to figure out our layout!
+
+### `MiamiEffect`
+
+Applies a color effect to the keyboard, inspired by the popular Miami keyset:
+
+![Miami](extras/MiamiEffect.png)
+
+Alphas, punctuation, numbers, the space bar, the numbers and the dot on the
+keypad, and half the function keys will be in a cyan-ish color, the rest in
+magenta.
+
+### `JukeBoxEffect`
+
+Applies a color effect to the keyboard, inspired by the JukeBox keyset:
+
+![Jukebox](extras/JukeboxEffect.png)
+
+Alphas, punctuation, numbers, the space bar, the numbers and the dot on the
+keypad, and half the function keys will be in a beige-ish color, the rest in
+light green, except for the `Esc` key, which will be in red.
+
+An alternative color scheme exists under the `JukeBoxAlternateEffect` name,
+where the light green and red colors are swapped.
+
+## Plugin methods
+
+The plugin provides a single method on each of the included effect objects:
+
+### `.activate()`
+
+> When called, immediately activates the effect. Mostly useful in the `setup()`
+> method of the Sketch, or in macros that are meant to switch to the selected
+> effect, no matter where we are in the list.
+
+## Further reading
+
+Starting from the [example][plugin:example] is the recommended way of getting
+started with the plugin.
+
+ [plugin:example]: https://github.com/Akela-Plugins/Akela-LEDEffects/blob/master/examples/LEDEffects/LEDEffects.ino
---
+++ src/Akela/TriColor.cpp
@@ -0,0 +1,69 @@
+/* -*- mode: c++ -*-
+ * Akela -- Animated Keyboardio Extension Library for Anything
+ * Copyright (C) 2016, 2017  Gergely Nagy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Akela-LEDEffects.h>
+
+namespace Akela {
+  namespace LEDEffects {
+
+    TriColor::TriColor (cRGB baseColor, cRGB modColor, cRGB escColor) {
+      this->baseColor = baseColor;
+      this->modColor = modColor;
+      this->escColor = escColor;
+      previousLayerState = 0xffffffff;
+    }
+
+    void
+    TriColor::update (void) {
+      uint8_t layerState = Layer.getLayerState ();
+      if (previousLayerState == layerState)
+        return;
+      previousLayerState = layerState;
+
+      for (uint8_t r = 0; r < ROWS; r++) {
+        for (uint8_t c = 0; c < COLS; c++) {
+          Key k = Layer.lookup (r, c);
+
+          // Special keys are always modColor
+          if (k.flags != 0) {
+            led_set_crgb_at (r, c, modColor);
+            continue;
+          }
+
+          cRGB color = modColor;
+
+          switch (k.keyCode) {
+          case Key_A.keyCode ... Key_0.keyCode:
+          case Key_Space.keyCode:
+          case Key_Minus.keyCode ... Key_Slash.keyCode:
+          case Key_Keypad1.keyCode ... Key_KeypadDot.keyCode:
+          case Key_F1.keyCode ... Key_F4.keyCode:
+          case Key_F9.keyCode ... Key_F12.keyCode:
+            color = baseColor;
+            break;
+          case Key_Esc.keyCode:
+            color = escColor;
+            break;
+          }
+
+          led_set_crgb_at (r, c, color);
+        }
+      }
+    }
+  };
+};
