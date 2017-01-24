7f94af85b7c333b5e791594b07de3f5e6c0ee201
Gergely Nagy <algernon@madhouse-project.org>
2017-01-24 20:22:10 +0100

    example: Combine the example with Stalker for a better effect
    Use Stalker and the BlazingTrail effect to go through all keys as an example.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- examples/GhostInTheFirmware/GhostInTheFirmware.ino
+++ examples/GhostInTheFirmware/GhostInTheFirmware.ino
@@ -17,26 +17,27 @@
  */
 
 #include <Akela-GhostInTheFirmware.h>
+#include <Akela-LED-Stalker.h>
 #include <Keyboardio-Macros.h>
 
 const Key keymaps[][ROWS][COLS] PROGMEM = {
   [0] = KEYMAP_STACKED
   (
-   Key_LEDEffectNext, Key_1, Key_2, Key_3, Key_4, Key_5, M(0),
-   Key_Backtick,      Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,        Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDn,        Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Esc,
+   Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, M(0),
+   Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey,
+   Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey,
+   Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey,
 
-   Key_LCtrl, Key_Backspace, Key_LGUI, Key_LShift,
-   Key_skip,
+   Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey,
+   Key_NoKey,
 
-   Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
-   Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
-              Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
-   Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
+   Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey,
+   Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey,
+              Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey,
+   Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey,
 
-   Key_RShift, Key_RAlt, Key_Space, Key_RCtrl,
-   Key_skip
+   Key_NoKey, Key_NoKey, Key_NoKey, Key_NoKey,
+   Key_NoKey
    ),
 };
 
@@ -48,7 +49,71 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
 }
 
 static const Akela::GhostInTheFirmware::GhostKey ghostKeys[] PROGMEM = {
-  {0, 0, 10},
+  {0, 6, 5},
+  {0, 5, 5},
+  {0, 4, 5},
+  {0, 3, 5},
+  {0, 2, 5},
+  {0, 1, 5},
+  {0, 0, 5},
+  {1, 0, 5},
+  {1, 1, 5},
+  {1, 2, 5},
+  {1, 3, 5},
+  {1, 4, 5},
+  {1, 5, 5},
+  {1, 6, 5},
+  {2, 6, 5},
+  {2, 5, 5},
+  {2, 4, 5},
+  {2, 3, 5},
+  {2, 2, 5},
+  {2, 1, 5},
+  {2, 0, 5},
+  {3, 0, 5},
+  {3, 1, 5},
+  {3, 3, 5},
+  {3, 4, 5},
+  {3, 5, 5},
+  {0, 7, 5},
+  {1, 7, 5},
+  {2, 7, 5},
+  {3, 7, 5},
+  {3, 6, 5},
+
+  {3, 9, 5},
+  {3, 8, 5},
+  {2, 8, 5},
+  {1, 8, 5},
+  {0, 8, 5},
+  {3, 10, 5},
+  {3, 11, 5},
+  {3, 12, 5},
+  {3, 13, 5},
+  {3, 14, 5},
+  {3, 15, 5},
+  {2, 15, 5},
+  {2, 14, 5},
+  {2, 13, 5},
+  {2, 12, 5},
+  {2, 11, 5},
+  {2, 10, 5},
+  {2, 9, 5},
+  {1, 9, 5},
+  {1, 10, 5},
+  {1, 11, 5},
+  {1, 12, 5},
+  {1, 13, 5},
+  {1, 14, 5},
+  {1, 15, 5},
+  {0, 15, 5},
+  {0, 14, 5},
+  {0, 13, 5},
+  {0, 12, 5},
+  {0, 11, 5},
+  {0, 10, 5},
+  {0, 9, 5},
+
   {0, 0, 0}
 };
 
@@ -56,7 +121,10 @@ void setup () {
   Keyboardio.setup (KEYMAP_SIZE);
 
   GhostInTheFirmware.configure (ghostKeys);
-  Keyboardio.use (&GhostInTheFirmware, &Macros, NULL);
+  StalkerEffect.configure (STALKER (BlazingTrail, NULL));
+
+  Keyboardio.use (&GhostInTheFirmware, &StalkerEffect, &Macros, NULL);
+
 }
 
 void loop () {
