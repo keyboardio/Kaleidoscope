5df5b209d7f11c885e2946889d0a7deb805ad550
Gergely Nagy <algernon@madhouse-project.org>
2017-01-27 11:11:51 +0100

    Lifted out from Keyboardio-LEDControl
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ README.md
@@ -0,0 +1,6 @@
+# Keyboardio-LEDEffect-Numlock
+
+This is a plugin for [KeyboardioFirmware][fw], adding an LED effect for when
+Num-lock is active.
+
+ [fw]: https://github.com/keyboardio/KeyboardioFirmware
---
+++ src/Keyboardio-LEDEffect-Numlock.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Keyboardio-LEDControl.h"
+#include "LEDUtils.h"
+
+class LEDNumlock : LEDMode {
+ public:
+  LEDNumlock (uint8_t numpadIndex);
+
+  virtual void begin (void) final;
+
+  virtual void update (void) final;
+  virtual void setup (void) final;
+
+ private:
+  static void loopHook (bool postClear);
+};
