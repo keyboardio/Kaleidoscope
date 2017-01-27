04902c903c05f1bd0d4728fa67cdfb67992779b9
Gergely Nagy <algernon@madhouse-project.org>
2017-01-27 11:11:48 +0100

    Lifted out from Keyboardio-LEDControl
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ README.md
@@ -0,0 +1,5 @@
+# Keyboardio-LEDEffect-Breathe
+
+This is a plugin for [KeyboardioFirmware][fw], adding a breathe LED effect.
+
+ [fw]: https://github.com/keyboardio/KeyboardioFirmware
---
+++ src/Keyboardio-LEDEffect-Breathe.cpp
@@ -0,0 +1,12 @@
+#include "Keyboardio-LEDEffect-Breathe.h"
+
+LEDBreatheEffect_::LEDBreatheEffect_ (void) {
+}
+
+void
+LEDBreatheEffect_::update (void) {
+  cRGB color = breath_compute();
+  LEDControl.set_all_leds_to (color);
+}
+
+LEDBreatheEffect_ LEDBreatheEffect;
