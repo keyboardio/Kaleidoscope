66f61cad78fac9c8995279e5a2e64cc53c51483d
Jesse Vincent <jesse@keyboard.io>
2017-02-08 16:05:32 -0800

    Add in indentation guards around the AppSwitcher stacked keylayout example

--- examples/AppSwitcher/AppSwitcher.ino
+++ examples/AppSwitcher/AppSwitcher.ino
@@ -21,26 +21,29 @@
 
 #include "Macros.h"
 
+/* *INDENT-OFF* */
 const Key keymaps[][ROWS][COLS] PROGMEM = {
-    [0] = KEYMAP_STACKED
-    (
-        Key_NoKey,    Key_1, Key_2, Key_3, Key_4, Key_5, Key_NoKey,
-        Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-        Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-        Key_PageDn,   Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Esc,
+  [0] = KEYMAP_STACKED
+  (
+   Key_NoKey,    Key_1, Key_2, Key_3, Key_4, Key_5, Key_NoKey,
+   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_PageDn,   Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Esc,
 
-        Key_LCtrl, Key_Backspace, Key_LGUI, Key_LShift,
-        M(M_APPSWITCH),
+   Key_LCtrl, Key_Backspace, Key_LGUI, Key_LShift,
+   M(M_APPSWITCH),
 
-        Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
-        Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
-        Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
-        Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
+   Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
+   Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
+              Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
+   Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
 
-        Key_RShift, Key_RAlt, Key_Space, Key_RCtrl,
-        M(M_APPCANCEL)
-    ),
+   Key_RShift, Key_RAlt, Key_Space, Key_RCtrl,
+   M(M_APPCANCEL)
+   ),
 };
+/* *INDENT-ON* */
+
 
 const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
     switch (macroIndex) {
