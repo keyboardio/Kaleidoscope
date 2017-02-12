23aba7840236d06b9356bd41d94f669523fc0b4e
Jesse Vincent <jesse@keyboard.io>
2017-02-11 19:45:21 -0800

    Add some missing prototypes to the class definition

--- src/Kaleidoscope-Model01-TestMode.h
+++ src/Kaleidoscope-Model01-TestMode.h
@@ -12,7 +12,9 @@ class TestMode_ : public KaleidoscopePlugin {
     static void run_tests();
     static void test_leds();
     static void test_matrix();
+    static void wait_for_keypress();
     static void loopHook(bool postClear);
+    static void set_leds(uint8_t r, uint8_t g, uint8_t b);
 };
 
 extern TestMode_ TestMode;
