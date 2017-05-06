a7ca8a11ffa0eb2db0114010c8fe349bad5ddbcc
Wez Furlong <wez@wezfurlong.org>
2017-05-05 21:39:38 -0700

    move mouse button defs to their own header file
    These defines were duplicated; let's have just one
    copy.   These symbols also collide with the teensy
    headers, which is why they're on my radar.  Will
    figure out what to do about that later.

--- plugins/KeyboardioHID/src/MultiReport/Mouse.h
+++ plugins/KeyboardioHID/src/MultiReport/Mouse.h
@@ -28,15 +28,7 @@ THE SOFTWARE.
 #include "PluggableUSB.h"
 #include "HID.h"
 #include "HID-Settings.h"
-
-#define MOUSE_LEFT		(1 << 0)
-#define MOUSE_RIGHT		(1 << 1)
-#define MOUSE_MIDDLE	(1 << 2)
-#define MOUSE_PREV		(1 << 3)
-#define MOUSE_NEXT		(1 << 4)
-// actually this mouse report has 8 buttons (for smaller descriptor)
-// but the last 3 wont do anything from what I tested
-#define MOUSE_ALL (MOUSE_LEFT | MOUSE_RIGHT | MOUSE_MIDDLE | MOUSE_PREV | MOUSE_NEXT)
+#include "MouseButtons.h"
 
 typedef union {
     // Mouse report: 8 buttons, position, wheel
