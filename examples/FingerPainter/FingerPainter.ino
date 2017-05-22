f4e9b476a06e1fbaadff47cc625a20b3d7462462
Jesse Vincent <jesse@keyboard.io>
2017-05-22 09:14:27 -0700

    Update to new Key API

--- examples/FingerPainter/FingerPainter.ino
+++ examples/FingerPainter/FingerPainter.ino
@@ -30,9 +30,9 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    M(0),              Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick,      Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,        Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDn,        Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Esc,
+   Key_PageDown,      Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
 
-   Key_LCtrl, Key_Backspace, Key_LGUI, Key_LShift,
+   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    Key_skip,
 
    Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
@@ -40,7 +40,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
               Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
    Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
 
-   Key_RShift, Key_RAlt, Key_Space, Key_RCtrl,
+   Key_RightShift, Key_RightAlt, Key_Spacebar, Key_RightControl,
    Key_skip
   ),
 };
