c3dbe52f723ba8ac93cfdecf0d1e6880644b3b42
Jesse Vincent <jesse@keyboard.io>
2017-05-22 13:12:20 -0700

    Bring example up to new API

--- examples/Leader/Leader.ino
+++ examples/Leader/Leader.ino
@@ -22,12 +22,12 @@
 const Key keymaps[][ROWS][COLS] PROGMEM = {
   [0] = KEYMAP_STACKED
   (
-   Key_NoKey,    Key_1, Key_2, Key_3, Key_4, Key_5, Key_NoKey,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDn,   Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Esc,
+   Key_NoKey, Key_1, Key_2, Key_3, Key_4, Key_5, Key_NoKey,
+   Key_Backtick,      Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+   Key_PageUp,        Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_PageDown,      Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
 
-   Key_LCtrl, Key_Backspace, Key_LGUI, Key_LShift,
+   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    LEAD(0),
 
    Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
@@ -35,9 +35,9 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
               Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
    Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
 
-   Key_RShift, Key_RAlt, Key_Space, Key_RCtrl,
+   Key_RightShift, Key_RightAlt, Key_Spacebar, Key_RightControl,
    LEAD(0)
-   ),
+  ),
 };
 
 static void leaderTestA (uint8_t seqIndex) {
