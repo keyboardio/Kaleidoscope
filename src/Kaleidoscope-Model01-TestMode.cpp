4ae6bff15df6024c09c4eac3715ce1dbf5209854
Jesse Vincent <jesse@keyboard.io>
2017-06-03 23:49:58 -0700

    astyle to pad operators

--- src/Kaleidoscope-Model01-TestMode.cpp
+++ src/Kaleidoscope-Model01-TestMode.cpp
@@ -13,9 +13,9 @@ TestMode_::TestMode_(void) {
 
 
 void TestMode_::begin(void) {
-  red.r=101;
-  blue.b=101;
-  green.g=101;
+  red.r = 101;
+  blue.b = 101;
+  green.g = 101;
   loop_hook_use(this->loopHook);
 
 }
@@ -43,7 +43,7 @@ void TestMode_::wait_for_keypress() {
 }
 
 void TestMode_::set_leds(uint8_t r, uint8_t g, uint8_t b) {
-  LEDControl.set_all_leds_to(r,g,b);
+  LEDControl.set_all_leds_to(r, g, b);
   LEDControl.led_sync();
   wait_for_keypress();
 
@@ -51,23 +51,23 @@ void TestMode_::set_leds(uint8_t r, uint8_t g, uint8_t b) {
 
 void TestMode_::test_leds(void) {
   // make all LEDs dim red
-  set_leds(50,0,0);
+  set_leds(50, 0, 0);
   // make all LEDs dim blue
-  set_leds(0,50,0);
+  set_leds(0, 50, 0);
   // make all LEDs dim green
-  set_leds(0,0,50);
+  set_leds(0, 0, 50);
   // make all LEDs dim white
-  set_leds(50,50,50);
+  set_leds(50, 50, 50);
   // make all the LEDs bright red
-  set_leds(200,0,0);
+  set_leds(200, 0, 0);
   // make all the LEDs bright green
-  set_leds(0,200,0);
+  set_leds(0, 200, 0);
   // make all the LEDs bright blue
-  set_leds(0,0,200);
+  set_leds(0, 0, 200);
   // make all the LEDs bright white (1.6A)
-  set_leds(160,160,160);
+  set_leds(160, 160, 160);
   // rainbow for 10 seconds
-  for (auto i=0; i<1000; i++) {
+  for (auto i = 0; i < 1000; i++) {
     LEDRainbowEffect.update();
     LEDControl.led_sync();
   }
@@ -78,7 +78,7 @@ void TestMode_::test_leds(void) {
 
 
 void TestMode_::test_matrix() {
-  LEDControl.set_all_leds_to(50,0,0);
+  LEDControl.set_all_leds_to(50, 0, 0);
   while (1) {
     KeyboardHardware.read_matrix();
     if (KeyboardHardware.leftHandState.all == (R0C0 | R0C6 | R3C6)) {
@@ -87,32 +87,32 @@ void TestMode_::test_matrix() {
     for (byte row = 0; row < 4; row++) {
       for (byte col = 0; col < 8; col++) {
 
-        uint8_t keynum = (row*8)+(col);
+        uint8_t keynum = (row * 8) + (col);
 
         uint8_t keyState = (bitRead(KeyboardHardware.previousLeftHandState.all, keynum) << 0) |
                            (bitRead(KeyboardHardware.leftHandState.all,         keynum) << 1);
 
-        if (keyState ==3) {
+        if (keyState == 3) {
           Serial.print(" Key: ");
           Serial.print(keynum);
           Serial.print(" value ");
           Serial.println(keyState);
-          KeyboardHardware.led_set_crgb_at(row, 7-col, green);
-        } else if (keyState ==1) {
-          KeyboardHardware.led_set_crgb_at(row,7-col, blue);
+          KeyboardHardware.led_set_crgb_at(row, 7 - col, green);
+        } else if (keyState == 1) {
+          KeyboardHardware.led_set_crgb_at(row, 7 - col, blue);
         }
 
         keyState = (bitRead(KeyboardHardware.previousRightHandState.all, keynum) << 0) |
                    (bitRead(KeyboardHardware.rightHandState.all,         keynum) << 1);
 
-        if (keyState ==3) {
+        if (keyState == 3) {
           Serial.print(" Key: ");
           Serial.print(keynum);
           Serial.print(" value ");
           Serial.println(keyState);
-          KeyboardHardware.led_set_crgb_at(row, 15-col, green);
-        } else if (keyState ==1) {
-          KeyboardHardware.led_set_crgb_at(row,15-col, blue);
+          KeyboardHardware.led_set_crgb_at(row, 15 - col, green);
+        } else if (keyState == 1) {
+          KeyboardHardware.led_set_crgb_at(row, 15 - col, blue);
         }
       }
     }
