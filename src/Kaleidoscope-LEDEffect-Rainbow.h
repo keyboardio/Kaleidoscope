d1fc67e45db08d8f3fab7ac40700daa89635890d
Jesse Vincent <jesse@keyboard.io>
2017-06-12 09:28:50 +0800

    deconstify things we think we might want to let users configure

--- src/Kaleidoscope-LEDEffect-Rainbow.h
+++ src/Kaleidoscope-LEDEffect-Rainbow.h
@@ -13,12 +13,12 @@ class LEDRainbowEffect_ : public LEDMode {
  private:
   uint16_t rainbow_hue = 0;   //stores 0 to 614
 
-  static const uint8_t rainbow_steps = 1; //number of hues we skip in a 360 range per update
+  static uint8_t rainbow_steps = 1; //number of hues we skip in a 360 range per update
   uint16_t rainbow_current_ticks = 0;
-  static const uint16_t rainbow_ticks = 10; //delays between update
+  static uint16_t rainbow_ticks = 10; //delays between update
 
-  static const byte rainbow_saturation = 255;
-  static const byte rainbow_value = 50;
+  static byte rainbow_saturation = 255;
+  static byte rainbow_value = 50;
 
 };
 
@@ -34,12 +34,12 @@ class LEDRainbowWaveEffect_ : public LEDMode {
  private:
   uint16_t rainbow_hue = 0;   //stores 0 to 614
 
-  static const uint8_t rainbow_wave_steps = 1; //number of hues we skip in a 360 range per update
+  static uint8_t rainbow_wave_steps = 1; //number of hues we skip in a 360 range per update
   uint16_t rainbow_current_ticks = 0;
-  static const uint16_t rainbow_wave_ticks = 10; //delays between update
+  static uint16_t rainbow_wave_ticks = 10; //delays between update
 
-  static const byte rainbow_saturation = 255;
-  static const byte rainbow_value = 50;
+  static byte rainbow_saturation = 255;
+  static byte rainbow_value = 50;
 };
 
 extern LEDRainbowWaveEffect_ LEDRainbowWaveEffect;
