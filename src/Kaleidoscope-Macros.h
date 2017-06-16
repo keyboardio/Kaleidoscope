23fec81b2868bfb4e6bdbc5eacd00dca7c70047e
Gergely Nagy <algernon@madhouse-project.org>
2017-06-16 09:10:13 +0200

    Allow type() to take any number of arguments
    To make it easier to type a set of strings, apply some template magic to the
    `Macros.type()` method. The same trick that is used in `Kaleidoscope.use()`.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/Kaleidoscope-Macros.h
+++ src/Kaleidoscope-Macros.h
@@ -14,7 +14,21 @@ class Macros_ : public KaleidoscopePlugin {
   void begin(void) final;
 
   void play(const macro_t *macro_p);
+
+  /* What follows below, is a bit of template magic that allows us to use
+     Macros.type() with any number of arguments, without having to use a
+     sentinel. See the comments on Kaleidoscope.use() for more details - this is
+     the same trick.
+   */
+  inline const macro_t *type() {
+    return MACRO_NONE;
+  }
   const macro_t *type(const char *string);
+  template <typename... Strings>
+  const macro_t *type(const char *first, Strings&&... strings) {
+    type(first);
+    return type(strings...);
+  }
 
   static byte row, col;
 
