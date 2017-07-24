3154f7c997d74d745783f63d5643ed03c36c60bc
Jesse Vincent <jesse@keyboard.io>
2017-07-24 13:42:42 -0700

    Add "correct" camelCase versions of the keyswitch state macros to ease transition to the new API
    We'll remove the deprecated versions after the core greps clean

--- src/keyswitch_state.h
+++ src/keyswitch_state.h
@@ -7,6 +7,14 @@
 #define IS_PRESSED  B00000010
 #define WAS_PRESSED B00000001
 
+#define keyWasPressed(keyState) ((keyState) & WAS_PRESSED)
+#define keyIsPressed(keyState)  ((keyState) & IS_PRESSED)
+#define keyToggledOn(keyState)  (keyIsPressed(keyState) && ! keyWasPressed(keyState))
+#define keyToggledOff(keyState) (keyWasPressed(keyState) && ! keyIsPressed(keyState))
+
+
+// Deprecated - Remove once the core has transitioned
+
 #define key_was_pressed(keyState) ((keyState) & WAS_PRESSED)
 #define key_is_pressed(keyState)  ((keyState) & IS_PRESSED)
 #define key_toggled_on(keyState)  (key_is_pressed(keyState) && ! key_was_pressed(keyState))
