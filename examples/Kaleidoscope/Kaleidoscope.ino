a4dfe289b36eaa3ba9c1f29c2238f51ea7dc9d96
Jesse Vincent <jesse@keyboard.io>
2017-10-02 22:23:54 -0700

    Update the example to match the new numlock api

--- examples/Kaleidoscope/Kaleidoscope.ino
+++ examples/Kaleidoscope/Kaleidoscope.ino
@@ -49,7 +49,7 @@
 )
 
 #define QWERTY KEYMAP ( \
-        ___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,         ___,        Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_ToggleNumlock, \
+        ___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,         ___,        Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_KeypadNumLock, \
         Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,                   Key_Enter,  Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals, \
         Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,                                        Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote, \
         Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,                 ___,       Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus, \
@@ -74,10 +74,6 @@ static kaleidoscope::LEDSolidColor solidIndigo(0, 0, 100);
 static kaleidoscope::LEDSolidColor solidViolet(70, 0, 60);
 
 const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
-  if (macroIndex == TOGGLENUMLOCK && keyToggledOn(keyState)) {
-    return NumLock.toggle();
-  }
-
   if (macroIndex == 1 && keyToggledOn(keyState)) {
     Serial.print("Keyboard.IO keyboard driver v0.00");
     return MACRO(I(25),
