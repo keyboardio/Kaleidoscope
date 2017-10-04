27caf6e95867491398057116a51057252a84ae84
Gergely Nagy <kaleidoscope@gergo.csillger.hu>
2017-10-04 11:47:29 +0200

    Drop the _hook_append and _hook_replace aliases
    Neither of these are used in any plugin, within Arduino-Boards or outside of it.
    We keep the `_hook_use` aliases, because there are a few users of it outside of
    Arduino-Boards.
    
    Signed-off-by: Gergely Nagy <kaleidoscope@gergo.csillger.hu>

--- src/Kaleidoscope.h
+++ src/Kaleidoscope.h
@@ -122,9 +122,4 @@ extern Kaleidoscope_ Kaleidoscope;
 /* -- DEPRECATED aliases; remove them when there are no more users. -- */
 
 #define event_handler_hook_use(hook) Kaleidoscope.useEventHandlerHook(hook);
-#define event_handler_hook_append(hook) Kaleidoscope.appendEventHandlerHook(hook)
-#define event_handler_hook_replace(oldHook, newHook) Kaleidoscope.replaceEventHandlerHook(oldHook, newHook)
-
 #define loop_hook_use(hook) Kaleidoscope.useLoopHook(hook)
-#define loop_hook_append(hook) Kaleidoscope.appendLoopHook(hook)
-#define loop_hook_replace(oldHook, newHook) Kaleidoscope.replaceLoopHook(oldHook, newHook)
