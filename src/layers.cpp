f0d0bc490c55dd2a3d09058d1f20aca37051b344
Michael Richters <gedankenexperimenter@gmail.com>
2017-11-01 12:05:54 -0500

    Rearranged and commented Layer.on() and Layer.off()
    The boolean wasOn was unnecessary, and there was no need to call
    bitSet() (or bitClear(), in the case of Layer.off()) if the test
    passed. Mostly, I just added a few explanatory comments.

--- src/layers.cpp
+++ src/layers.cpp
@@ -125,29 +125,37 @@ void Layer_::move(uint8_t layer) {
 }
 
 void Layer_::on(uint8_t layer) {
-  bool wasOn = isOn(layer);
+  // If the target layer was off, turn it on:
+  if (!isOn(layer)) {
+    // Turn on its bit in LayerState
+    bitSet(LayerState, layer);
 
-  bitSet(LayerState, layer);
-  if (layer > highestLayer)
-    updateHighestLayer();
+    // If the target layer is above the previous highest active layer,
+    // update highestLayer
+    if (layer > highestLayer)
+      updateHighestLayer();
 
-  /* If the layer did turn on, update the keymap cache. See layers.h for an
-   * explanation about the caches we have. */
-  if (!wasOn)
+    // Update the keymap cache (but not liveCompositeKeymap; that gets
+    // updated separately, when keys toggle on or off. See layers.h
     updateActiveLayers();
+  }
 }
 
 void Layer_::off(uint8_t layer) {
-  bool wasOn = isOn(layer);
+  // If the target layer was on, turn it off:
+  if (isOn(layer)) {
+    // Turn off its bit in LayerState
+    bitClear(LayerState, layer);
 
-  bitClear(LayerState, layer);
-  if (layer == highestLayer)
-    updateHighestLayer();
+    // If the target layer was the previous highest active layer,
+    // update highestLayer
+    if (layer == highestLayer)
+      updateHighestLayer();
 
-  /* If the layer did turn off, update the keymap cache. See layers.h for an
-   * explanation about the caches we have. */
-  if (wasOn)
+    // Update the keymap cache (but not liveCompositeKeymap; that gets
+    // updated separately, when keys toggle on or off. See layers.h
     updateActiveLayers();
+  }
 }
 
 boolean Layer_::isOn(uint8_t layer) {
