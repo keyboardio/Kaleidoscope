536073ee4c786a5e7d02705402677cc5b105b3dc
Michael Richters <gedankenexperimenter@gmail.com>
2017-11-01 14:54:09 -0500

    Make astyle happy

--- src/layers.cpp
+++ src/layers.cpp
@@ -126,7 +126,8 @@ void Layer_::move(uint8_t layer) {
 
 void Layer_::on(uint8_t layer) {
   // If the target layer was already on, return
-  if (bitRead(LayerState, layer)) return;
+  if (bitRead(LayerState, layer))
+    return;
 
   // Otherwise, turn on its bit in LayerState
   bitSet(LayerState, layer);
@@ -143,7 +144,8 @@ void Layer_::on(uint8_t layer) {
 
 void Layer_::off(uint8_t layer) {
   // If the target layer was already off, return
-  if (bitRead(LayerState, layer)) return;
+  if (bitRead(LayerState, layer))
+    return;
   
   // Turn off its bit in LayerState
   bitClear(LayerState, layer);
