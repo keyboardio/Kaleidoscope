ac354318d363d09f8df5bfe96b503377aa96eef6
Michael Richters <gedankenexperimenter@gmail.com>
2017-11-24 18:38:20 -0600

    Basic skeleton code

---
+++ src/Kaleidoscope/Qukeys.cpp
@@ -0,0 +1,35 @@
+/* -*- mode: c++ -*-
+ * Kaleidoscope-Qukeys -- Assign two keycodes to a single key
+ * Copyright (C) 2017  Michael Richters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Kaleidoscope-Qukeys.h>
+#include <kaleidoscope/hid.h>
+
+#ifdef ARDUINO_VIRTUAL
+#define debug_print(...) printf(__VA_ARGS__)
+#else
+#define debug_print(...)
+#endif
+
+
+namespace kaleidoscope {
+
+// constructor
+Qukeys::Qukeys(void) {
+}
+
+}
