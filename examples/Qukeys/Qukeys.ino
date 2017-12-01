d12a4870fe19056cec94e877a4162f4ac9880fff
Michael Richters <gedankenexperimenter@gmail.com>
2017-11-30 23:46:57 -0600

    Added `setTimeout()` function to set the time limit in sketch

--- examples/Qukeys/Qukeys.ino
+++ examples/Qukeys/Qukeys.ino
@@ -33,6 +33,7 @@ void setup() {
     kaleidoscope::Qukey(0, 2, 3, Key_LeftControl),  // D/ctrl
     kaleidoscope::Qukey(0, 2, 4, Key_LeftShift)     // F/shift
 	 )
+  Qukeys.setTimeout(200);
 
   Kaleidoscope.setup();
 }
