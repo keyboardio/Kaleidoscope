6e57b9868faa8e492a817a73af779f93111b2755
Michael Richters <gedankenexperimenter@gmail.com>
2017-12-02 00:11:03 -0600

    Added a macro to toggle Qukeys in the example
    - also updated the keymaps definition

--- examples/Qukeys/Qukeys.ino
+++ examples/Qukeys/Qukeys.ino
@@ -2,8 +2,9 @@
 
 #include <Kaleidoscope.h>
 #include <Kaleidoscope-Qukeys.h>
+#include <Kaleidoscope-Macros.h>
 
-const Key keymaps[][ROWS][COLS] PROGMEM = {
+KEYMAPS(
   [0] = KEYMAP_STACKED
   (
     Key_NoKey,    Key_1, Key_2, Key_3, Key_4, Key_5, Key_NoKey,
@@ -14,14 +15,24 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
     Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
     ___,
 
-    Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
+    M(MACRO_TOGGLE_QUKEYS),  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
     Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
     Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
     Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
 
     Key_RightShift, Key_RightAlt, Key_Spacebar, Key_RightControl,
     ___),
-};
+)
+
+// Defining a macro (on the "any" key: see above) to toggle Qukeys on and off
+const macro_t *macroAction(uint8_t macro_index, uint8_t key_state) {
+  switch (macro_index) {
+  case MACRO_TOGGLE_QUKEYS:
+    Qukeys.toggle();
+    break;
+  }
+  return MACRO_NONE;
+}
 
 void setup() {
   // Use Qukeys
@@ -34,6 +45,9 @@ void setup() {
     kaleidoscope::Qukey(0, 2, 4, Key_LeftShift)     // F/shift
   )
   Qukeys.setTimeout(200);
+
+  // To toggle Qukeys off and on, we use a macro
+  Kaleidoscope.use(&Macros);
 
   Kaleidoscope.setup();
 }
