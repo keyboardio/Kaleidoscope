957d683c9375600727fb4e9efd043efdc5a0a047
Michael Richters <gedankenexperimenter@gmail.com>
2017-12-06 14:11:13 -0600

    Use <Key>.raw instead of just <Key> for comparison
    I'm not certain this matters, but using .raw is definitely correct

--- src/key_events.cpp
+++ src/key_events.cpp
@@ -75,7 +75,7 @@ void handleKeyswitchEvent(Key mappedKey, byte row, byte col, uint8_t keyState) {
       }
     }
 
-    if (mappedKey == Key_NoKey) {
+    if (mappedKey.raw == Key_NoKey.raw) {
       mappedKey = Layer.lookup(row, col);
     }
 
