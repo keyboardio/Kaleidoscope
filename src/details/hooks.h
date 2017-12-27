dad138897ef49c8ebfe50442dfb30e6bf3c9fac6
noseglasses <shinynoseglasses@gmail.com>
2017-12-27 19:05:20 +0100

    Added weak implementations of static members of class Hooks to enable backwards compatibility

--- src/details/hooks.h
+++ src/details/hooks.h
@@ -1,15 +1,26 @@
 #pragma once
 
+// Arduino.h provides access to the non-standard (Arduino custom)
+// type byte.
+//
+#include <Arduino.h>
+
+namespace kaleidoscope {
+class EventKey;
+union Key;
+}
+
+// Forward declaration required to enable friend declarations
+// in class Hooks.
+//
 class kaleidoscope_;
+extern void handleKeyswitchEvent(kaleidoscope::Key mappedKey, byte row, byte col, uint8_t keyState);
 
 namespace kaleidoscope {
 
-class KeyEvent;
-union Key;
-
 // The reason why the hook rooting entry point functions live within
-// class Hooks and not directly within a namespace is that we want
-// to restrict who is alowed to trigger hooks. This is mainly to prevent
+// class Hooks and not directly within a namespace is, that we want
+// to restrict who is allowed to trigger hooks, mainly to prevent
 // user code from calling hook methods.
 //
 // A note to maintainers: When you add new hooks that are supposed to
@@ -17,20 +28,24 @@ union Key;
 // below, just add a suitable friend declaration.
 
 class Hooks {
-  // The following friend declarations restrict access to the hook routing system.
 
-  // Kaleidoscope_ calls init, preReportHook and postReportHook
+  // The following friend declarations restrict access to
+  // the hook routing system.
+
+  // Kaleidoscope_ calls Hooks::init, Hooks::preReportHook
+  // and Hooks::postReportHook.
   //
   friend class Kaleidoscope_;
 
-  // ::handleKeyswitchEvent(...) calls eventHandlerHook
+  // ::handleKeyswitchEvent(...) calls Hooks::eventHandlerHook.
   //
-  friend void ::handleKeyswitchEvent(Key mappedKey, byte row, byte col, uint8_t keyState);
+  friend void ::handleKeyswitchEvent(kaleidoscope::Key mappedKey,
+                                     byte row, byte col, uint8_t keyState);
 
  private:
 
-  // The following private functions are just to be called by friend classes
-  // and functions.
+  // The following private functions are just to be called by classes
+  // and functions that are declared as friends above.
 
   static void init();
 
