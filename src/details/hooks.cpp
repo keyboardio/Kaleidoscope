2c6ec17d97fd57cc4ba90b286666103f3e546df5
noseglasses <flo@holf.de>
2017-12-27 20:04:10 +0100

    Update hooks.cpp
    Added a comment to explain the use of weak symbols in hooks.cpp

--- src/details/hooks.cpp
+++ src/details/hooks.cpp
@@ -2,6 +2,16 @@
 
 namespace kaleidoscope {
 
+// The following weak symbols are overwritten by
+// using the KALEIDOSCOPE_INIT_PLUGINS(...) macro
+// in the the firmware sketch. Their only purpose is
+// to provide backwards compatibility during the transition
+// from the old implementation of the hooking/plugin system
+// to the new one. The weak symbols ensure that there
+// are no undefined symbols if KALEIDOSCOPE_INIT_PLUGINS(...)
+// is not used. This allows legacy sketches to be used
+// during the transition phase.
+
 __attribute__((weak))
 void Hooks::init() {}
 
