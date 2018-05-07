7e84856ba45b41147f08d61bd28b6510c63e8732
Jesse Vincent <jesse@keyboard.io>
2018-05-06 19:27:30 -0700

    Rewrite comments and error messages in hook_signature_check.h for clarity

--- src/kaleidoscope_internal/hook_signature_check.h
+++ src/kaleidoscope_internal/hook_signature_check.h
@@ -3,42 +3,42 @@
 #include "macro_helpers.h"
 #include "plugin.h"
 
-// We use non-virtual methods for hooks. This reduces PROGMEM consumption and
-// runtime call overhead and allows for dead code elimination.
+// We use non-virtual methods for hooks. This reduces PROGMEM consumption,
+// runtime call overhead, and allows the compiler to a better job
+// removing dead card.
 //
-// This is not without additional costs as we
-// have to deal with the problem that hook methods in derived plugins are
-// hidden, rather than overridden (only virtual methods can be overridden).
-// This means that c++'s override keyword cannot be applied for our hooks.
+// The downside is that hook methods in derived plugins are hidden, 
+// rather than overridden. Because only virtual methods can be overridden,
+// we can't apply C++'s `override` keyword to hook methods.
 //
-// To prevent an accidental creation of a hook method with a wrong signature,
-// we compare the base class hook method's signature
-// with the signature of the hook method being actually called. If both types
-// differ, something went wrong and we output a verbose message that informs
-// the user about how the error can be fixed.
+// To make it easier to debug issues with plugins' hook method signatures,
+// the _VALIDATE_HOOK_METHOD_SIGNATURE macro compares a plugin's hook method 
+// signatures with those of the kaleidoscope::Plugin baseclass. If any 
+// differences are detected, it outputs a compile-time error message.
+// 
 //
-// What we would like to check is the following.
+// Ideally, we'd be able to use this (much simpler) code.
 //
 //    decltype(&::kaleidoscope::Plugin::fooHookMethod)
 //              == decltype(&childPlugin::fooHookMethod)
 //
-// Unfortunately, this is not possible with c++(-11) as it does not allow
-// to compare two (function-)types for equality.
+// Unfortunately, this is not possible with C++-11, as it does not allow
+// comparing function-types for equality. As a workaround, we can use
+// the trait class HookSignaturesMatch to perform the comparison.
 //
-// Because of this, the comparison is done based on the
-// traits class HookSignaturesMatch.
-// It defines the bool constant 'value' as true if both signatures
-// considering return value, call argument types and const specification
-// equal. We also allow for future const-hook methods here.
+// It defines the bool constant 'value' as true if both signatures match.
+// It checks the return value, argument types and const specification.
+// (As of this writing, we don't have any const hook method.)
 
 template<typename T1, typename T2>
 struct HookSignaturesMatch {
   static constexpr bool value = false;
 };
 
-// R: The return value, T1: Type of the first class (plugin),
-// T2: Type of the second class (plugin), HookArgs: Variadic types of
-// plugin hook arguments.
+// R: The return value, 
+// T1: Type of the first class (plugin),
+// T2: Type of the second class (plugin), 
+// HookArgs: Variadic types of plugin hook arguments.
 
 template<typename R, typename T1, typename T2, typename... HookArgs>
 struct HookSignaturesMatch<R(T1::*)(HookArgs...), R(T2::*)(HookArgs...)> {
@@ -47,39 +47,33 @@ struct HookSignaturesMatch<R(T1::*)(HookArgs...), R(T2::*)(HookArgs...)> {
 
 // Equivalent to allow for const-hooks, e.g. bool getFooHook() const
 
-template<typename R, typename T1, typename T2, typename... HookArgs>
-struct HookSignaturesMatch<R(T1::*)(HookArgs...) const, R(T2::*)(HookArgs...) const> {
+template<typename R, typename T1, typename T2, typename... HookArgs> struct
+HookSignaturesMatch<R(T1::*)(HookArgs...) const, R(T2::*)(HookArgs...) const> {
   static constexpr bool value = true;
 };
 
-// This template is instantiated when something goes wrong. It causes a
-// compiler error as it does not define a constant 'value'.
+// This template is instantiated when something goes wrong. 
+// Because it does not define a constant 'value', it triggers a compiler error.
 
-template<typename Plugin__, bool result>
-struct ___________Culprit_Plugin___________ {
-};
+template<typename Plugin__, bool result> struct
+  ___________Culprit_Plugin___________ { };
 
 // This specialization is instantiated when everything is ok.
-
-template<typename Plugin__>
-struct ___________Culprit_Plugin___________
-  <Plugin__, true> {
+template<typename Plugin__> struct 
+  ___________Culprit_Plugin___________ <Plugin__, true> {
   static constexpr bool value = true;
 };
 
-// The above specialized HookSignaturesMatch plugin class is instantiated
-// based on two types of pointers to the plugin base class hook method and
-// the derived method. If both pointer types and thus the signatures equal,
-// the first or second specialization is instantiated,
-// leading to its compile time constant 'value' being defined as true.
-// Else the unspecialized version of the template class is instantiated,
-// causing 'value' to be defined as false.
+// If the pointer types are the same, the signatures match, causing 
+// the first or second specialization to be instantiated. This makes
+// the causes the compile time constant `value` to be defined as `true`.
+// Otherwise, the unspecialized version of the template class is instantiated
+// and `value` is defined as `false`
 
 #define _VALIDATE_HOOK_METHOD_SIGNATURE(HOOK_METHOD, PLUGIN)                   \
 {                                                                       __NL__ \
-  /* Check it the two signatures equal. If not, the                     __NL__ \
-  * user has implemented a method with the same name but a              __NL__ \
-  * different (wrong) signature.                                        __NL__ \
+  /* Check if the signatures match. If not, the plugin has implemented  __NL__ \
+  * a method with a hook's name, but a different signature.             __NL__ \
   */                                                                    __NL__ \
   typedef HookSignaturesMatch<                                          __NL__ \
                      decltype(&::kaleidoscope::Plugin::HOOK_METHOD),    __NL__ \
@@ -88,32 +82,28 @@ struct ___________Culprit_Plugin___________
                                                                         __NL__ \
   static_assert(Check::value,                                           __NL__ \
     VERBOSE_STATIC_ASSERT_HEADER                                        __NL__ \
-   "\nOne of your plugins implemented a hook \"" #HOOK_METHOD "\""      __NL__ \
-   "\nwhose signature differs from the base class' hook signature."     __NL__ \
+   "\nOne of your plugins implemented the hook \"" #HOOK_METHOD "\","   __NL__ \
+   "\nbut its signature didn't match the base class."                   __NL__ \
    "\n"                                                                 __NL__ \
-   "\nPlease check the compiler messages above or below for an hint"    __NL__ \
-   "\nabout which plugin is the culprit. It will be marked by the"      __NL__ \
-   "\nstring"                                                           __NL__ \
+   "\nThe plugin with this issue will be marked in the compiler"        __NL__ \
+   "\noutput with the string:"                                          __NL__ \
    "\n"                                                                 __NL__ \
    "\n      ___________Culprit_Plugin___________."                      __NL__ \
    "\n"                                                                 __NL__ \
-   "\nThen check all the hook methods implemented by this plugin"       __NL__ \
-   "\nand compare them to the respective hook methods of"               __NL__ \
-   "\nbase class \"kaleidoscope::Plugin\"."                             __NL__ \
+   "\nYou should compare the hook methods implemented in this plugin"   __NL__ \
+   "\nto those in \"kaleidoscope::Plugin\"."                            __NL__ \
    "\n"                                                                 __NL__ \
-   "\nNote: Two method signatures equal if all their argument"          __NL__ \
-   "\n      types, the methods' const qualifiers and their"             __NL__ \
-   "\n      return values equal."                                       __NL__ \
+   "\nAll of the hook method's argument types, return values and"       __NL__ \
+   "\nconst qualifiers need to match."                                  __NL__ \
    "\n"                                                                 __NL__ \
     VERBOSE_STATIC_ASSERT_FOOTER                                        __NL__ \
   );                                                                    __NL__ \
                                                                         __NL__ \
   /* The following construct is necessary enable reporting of the       __NL__ \
-   * type of the plugin that implemented a hook with a wrong signature. __NL__ \
-   * This is necessary as it is not possible to include any non         __NL__ \
-   * literal string constans in the error message of a static_assert.   __NL__ \
+   * type of a plugin that implements a hook with an incorrect          __NL__ \
+   * because it's not possible to include any non-literal string        __NL__ \
+   * constant in a static_assert error message.                         __NL__ \
    */                                                                   __NL__ \
-  constexpr bool dummy =                                                __NL__ \
-    ___________Culprit_Plugin___________                                __NL__ \
+  constexpr bool dummy = ___________Culprit_Plugin___________           __NL__ \
        <PLUGIN, Check::value>::value;                                   __NL__ \
 }
