7389f5ac17b7d87133d4a38f089f50a4be2b4648
Gergely Nagy <algernon@keyboard.io>
2018-05-10 00:39:54 +0200

    renaming a few more things
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/kaleidoscope_internal/eventhandler_signature_check.h
+++ src/kaleidoscope_internal/eventhandler_signature_check.h
@@ -3,52 +3,50 @@
 #include "macro_helpers.h"
 #include "plugin.h"
 
-// We use non-virtual methods for hooks. This reduces RAM (and to a smaller
-// extent, PROGMEM) consumption, runtime call overhead, and allows the compiler
-// to a better job removing dead code.
+// We use non-virtual methods for event handlers. This reduces RAM (and to a
+// smaller extent, PROGMEM) consumption, runtime call overhead, and allows the
+// compiler to a better job removing dead code.
 //
-// The downside is that hook methods in derived plugins are hidden,
-// rather than overridden. Because only virtual methods can be overridden,
-// we can't apply C++'s `override` keyword to hook methods.
+// The downside is that event handler methods in derived plugins are hidden,
+// rather than overridden. Because only virtual methods can be overridden, we
+// can't apply C++'s `override` keyword to hook methods.
 //
-// To make it easier to debug issues with plugins' hook method signatures,
-// the _VALIDATE_HOOK_METHOD_SIGNATURE macro compares a plugin's hook method
-// signatures with those of the kaleidoscope::Plugin baseclass. If any
-// differences are detected, it outputs a compile-time error message.
-//
+// To make it easier to debug issues with plugins' event handler method
+// signatures, the _VALIDATE_EVENT_HANDLER_SIGNATURE macro compares a plugin's
+// event handler signatures with those of the kaleidoscope::Plugin baseclass. If
+// any differences are detected, it outputs a compile-time error message.
 //
 // Ideally, we'd be able to use this (much simpler) code.
 //
-//    decltype(&::kaleidoscope::Plugin::fooHookMethod)
-//              == decltype(&childPlugin::fooHookMethod)
+//    decltype(&::kaleidoscope::Plugin::fooEventHandler)
+//              == decltype(&childPlugin::fooEventHandler)
 //
 // Unfortunately, this is not possible with C++-11, as it does not allow
-// comparing function-types for equality. As a workaround, we can use
-// the trait class HookSignaturesMatch to perform the comparison.
+// comparing function-types for equality. As a workaround, we can use the trait
+// class EventHandlerSignaturesMatch to perform the comparison.
 //
 // It defines the bool constant 'value' as true if both signatures match.
 // It checks the return value, argument types and const specification.
 // (As of this writing, we don't have any const hook method.)
 
 template<typename T1, typename T2>
-struct HookSignaturesMatch {
+struct EventHandlerSignaturesMatch {
   static constexpr bool value = false;
 };
 
 // R: The return value,
 // T1: Type of the first class (plugin),
 // T2: Type of the second class (plugin),
-// HookArgs: Variadic types of plugin hook arguments.
+// EventHandlerArgs: Variadic types of plugin event handler arguments.
 
-template<typename R, typename T1, typename T2, typename... HookArgs>
-struct HookSignaturesMatch<R(T1::*)(HookArgs...), R(T2::*)(HookArgs...)> {
+template<typename R, typename T1, typename T2, typename... EventHandlerArgs>
+struct EventHandlerSignaturesMatch<R(T1::*)(EventHandlerArgs...), R(T2::*)(EventHandlerArgs...)> {
   static constexpr bool value = true;
 };
 
-// Equivalent to allow for const-hooks, e.g. bool getFooHook() const
-
-template<typename R, typename T1, typename T2, typename... HookArgs> struct
-HookSignaturesMatch<R(T1::*)(HookArgs...) const, R(T2::*)(HookArgs...) const> {
+// Equivalent to allow for const-eventhandlers, e.g. bool getFooEventHandler() const
+template<typename R, typename T1, typename T2, typename... EventHandlerArgs> struct
+EventHandlerSignaturesMatch<R(T1::*)(EventHandlerArgs...) const, R(T2::*)(EventHandlerArgs...) const> {
   static constexpr bool value = true;
 };
 
@@ -70,38 +68,38 @@ template<typename Plugin__> struct
 // Otherwise, the unspecialized version of the template class is instantiated
 // and `value` is defined as `false`
 
-#define _VALIDATE_HOOK_METHOD_SIGNATURE(HOOK_METHOD, PLUGIN)                   \
+#define _VALIDATE_EVENT_HANDLER_SIGNATURE(EVENTHANDLER, PLUGIN)                  \
 {                                                                         __NL__ \
   /* Check if the signatures match. If not, the plugin has implemented */ __NL__ \
   /* a method with a hook's name, but a different signature.           */ __NL__ \
-  typedef HookSignaturesMatch<                                            __NL__ \
-                     decltype(&::kaleidoscope::Plugin::HOOK_METHOD),      __NL__ \
-                     decltype(&PLUGIN::HOOK_METHOD)                       __NL__ \
+  typedef EventHandlerSignaturesMatch<                                    __NL__ \
+                     decltype(&::kaleidoscope::Plugin::EVENTHANDLER),     __NL__ \
+                     decltype(&PLUGIN::EVENTHANDLER)                      __NL__ \
                 > Check;                                                  __NL__ \
                                                                           __NL__ \
   static_assert(Check::value,                                             __NL__ \
     VERBOSE_STATIC_ASSERT_HEADER                                          __NL__ \
-   "\nOne of your plugins implemented the hook \"" #HOOK_METHOD "\","     __NL__ \
-   "\nbut its signature didn't match the base class."                     __NL__ \
+   "\nOne of your plugins implemented the \"" #EVENTHANDLER "\""          __NL__ \
+   "\nevent handler, but its signature didn't match the base class."      __NL__ \
    "\n"                                                                   __NL__ \
    "\nThe plugin with this issue will be marked in the compiler"          __NL__ \
    "\noutput with the string:"                                            __NL__ \
    "\n"                                                                   __NL__ \
    "\n      ___________Culprit_Plugin___________."                        __NL__ \
    "\n"                                                                   __NL__ \
-   "\nYou should compare the hook methods implemented in this plugin"     __NL__ \
+   "\nYou should compare the event handlers implemented in this plugin"   __NL__ \
    "\nto those in \"kaleidoscope::Plugin\"."                              __NL__ \
    "\n"                                                                   __NL__ \
-   "\nAll of the hook method's argument types, return values and"         __NL__ \
+   "\nAll of the event handler's argument types, return values and"       __NL__ \
    "\nconst qualifiers need to match."                                    __NL__ \
    "\n"                                                                   __NL__ \
     VERBOSE_STATIC_ASSERT_FOOTER                                          __NL__ \
   );                                                                      __NL__ \
                                                                           __NL__ \
   /* The following construct is necessary enable reporting of the      */ __NL__ \
-  /* type of a plugin that implements a hook with an incorrect         */ __NL__ \
-  /* because it's not possible to include any non-literal string       */ __NL__ \
-  /* constant in a static_assert error message.                        */ __NL__ \
+  /* type of a plugin that implements an event handler with an         */ __NL__ \
+  /* incorrect signature, because it's not possible to include any     */ __NL__ \
+  /* non-literal string constant in a static_assert error message.     */ __NL__ \
   constexpr bool dummy = ___________Culprit_Plugin___________             __NL__ \
        <PLUGIN, Check::value>::value;                                     __NL__ \
 }
--- src/kaleidoscope_internal/plugin_hooks.h
+++ src/kaleidoscope_internal/plugin_hooks.h
@@ -20,7 +20,7 @@
 #include "macro_helpers.h"
 #include "plugin.h"
 #include "kaleidoscope/hooks.h"
-#include "hook_signature_check.h"
+#include "eventhandler_signature_check.h"
 
 namespace kaleidoscope_internal {
 
@@ -36,21 +36,20 @@ namespace kaleidoscope_internal {
 
 // Some words about the design of hook rooting:
 //
-// The HookPoint class implements a compile-time loop over all plugins. This
-// loop is used to call a non virtual hook method of each plugin.
+// The EventHandlers class implements a compile-time loop over all plugins. This
+// loop is used to call a non virtual event handler of each plugin.
 //
-// The advantage of this approach against a solution with virtual
-// hook methods in classes derived from kaleidoscope::Plugin is a significant
-// reduction of virtual function calls and a reduction of the amount of
-// virtual function tables (vtables).
+// The advantage of this approach against a solution with virtual event handlers
+// in classes derived from kaleidoscope::Plugin is a significant reduction of
+// virtual function calls and a reduction of the amount of virtual function
+// tables (vtables).
 //
-// For every hook that is called by the firmware core, only one
-// ordinary function call (a static function of class Hooks) is invoked instead of
-// one for each plugin and hook. Plugins derived
-// from kaleidoscope::Plugin are not (dynamically) polymorphic classes
-// (not featuring virtual methods). Thus, classes do not need virtual tables
-// and instances no virtual pointers, which, as a consequence,
-// significantly reduces binary size.
+// For every handler that is called by the firmware core, only one ordinary
+// function call (a static function of class Hooks) is invoked instead of one
+// for each plugin and handler. Plugins derived from kaleidoscope::Plugin are
+// not (dynamically) polymorphic classes (not featuring virtual methods). Thus,
+// classes do not need virtual tables and instances no virtual pointers, which,
+// as a consequence, significantly reduces binary size.
 //
 // Note: A the time of writing this, the kaleidoscope::Plugin is still
 //       featuring a virtual begin() method to support legacy implementations
@@ -60,42 +59,43 @@ namespace kaleidoscope_internal {
 //       Those might both vanish when the begin() method would be removed from
 //       kaleidoscope::Plugin in future versions of the firmware.
 //
-// The call to hook methods through kaleidoscope::Hooks and
-// kaleidoscope_internal::HookPoint is templated to allow for compile time
-// static polymorphisms (hook methods of plugins not beeing virtual).
+// The call to event handlers through kaleidoscope::Hooks and
+// kaleidoscope_internal::EventHandlers is templated to allow for compile time
+// static polymorphisms (event handlers of plugins not beeing virtual).
 //
 // This approach is somewhat similar to duck typing in scripting languages like,
 // e.g. Python. Only the signature of a method counts and not the fact that the
 // class is actually derived from a specific base class. Strictly speaking,
 // plugins do not need to be derived from kaleidoscope::Plugin, but only need to
-// implement a set of hooks whose call signature matches.
+// implement a set of event handlers whose call signature matches.
 //
-// If a plugin implements a hook method (thereby hiding the equivalent method
-// of the plugin base class) the derived plugins's method is called instead
-// of the version of the hook method provided by base class kaleidoscope::Plugin.
-// This is possible as the hook method is not invoked via a base class ptr
-// or reference but via a reference to the actual derived plugin class.
-// Thus the compiler can see the actual type of the plugin at the point of
-// the hook method's invocation.
+// If a plugin implements an event handler (thereby hiding the equivalent method
+// of the plugin base class) the derived plugins's method is called instead of
+// the version of the event handler provided by base class kaleidoscope::Plugin.
+// This is possible as the event handler is not invoked via a base class ptr or
+// reference but via a reference to the actual derived plugin class. Thus the
+// compiler can see the actual type of the plugin at the point of the event
+// handler's invocation.
 //
-// HookPoint::apply() implements a compile time for-each loop over all plugins.
-// Under the assumption that only some (few) plugins implement a greater number
-// of hook methods and that there is only a limited number of plugins used in a
-// sketch, this approach is quite efficient both in terms of code size and
-// run-time. This is due to the compiler optimizing away any calls to hooks that
-// have not been implemented by plugins because the base class versions of these
-// hooks are mostly noops. The latter are easy to detect and eliminate by the
-// compiler.
+// EventHandlers::apply() implements a compile time for-each loop over all
+// plugins. Under the assumption that only some (few) plugins implement a
+// greater number of event handlers and that there is only a limited number of
+// plugins used in a sketch, this approach is quite efficient both in terms of
+// code size and run-time. This is due to the compiler optimizing away any calls
+// to methods that have not been implemented by plugins because the base class
+// versions of these are mostly noops. The latter are easy to detect and
+// eliminate by the compiler.
 //
-// Calling the plugins' hook methods is carried out via a call to one of the
-// static functions of class Hooks. Those functions cast the related hook method
-// on every registered plugin by using the HookPoint helper class.
+// Calling the plugins' event handlers is carried out via a call to one of the
+// static functions of class Hooks. Those functions cast the related event
+// handler on every registered plugin by using the EventHandlers helper class.
 
 // The _EVENT_HANDLER macro defines an auxiliary classes EventHandler_... that
-// invoke a specific plugin hook method with a provided set of method arguments.
-// The EventHandler_... classes are meant to be used by HookPoint's apply
-// function called in the static functions of kaleidoscope::Hooks to forward the
-// call to the hook methods of all registered plugins.
+// invoke a specific plugin event handler with a provided set of method
+// arguments. The EventHandler_... classes are meant to be used by
+// EventHandlers' apply function called in the static functions of
+// kaleidoscope::Hooks to forward the call to the event handlers of all
+// registered plugins.
 #define _EVENT_HANDLER(EVENT_HANDLER, SHOULD_ABORT_ON_CONSUMED_EVENT)            \
    struct EventHandler_##EVENT_HANDLER {                                  __NL__ \
                                                                           __NL__ \
@@ -104,9 +104,9 @@ namespace kaleidoscope_internal {
       }                                                                   __NL__ \
                                                                           __NL__ \
       template<typename Plugin__, typename... Args__>                     __NL__ \
-      static kaleidoscope::EventHandlerResult call(Plugin__ &plugin,          __NL__ \
+      static kaleidoscope::EventHandlerResult call(Plugin__ &plugin,      __NL__ \
                                                Args__&&... hook_args) {   __NL__ \
-         _VALIDATE_HOOK_METHOD_SIGNATURE(EVENT_HANDLER, Plugin__)         __NL__ \
+         _VALIDATE_EVENT_HANDLER_SIGNATURE(EVENT_HANDLER, Plugin__)       __NL__ \
                                                                           __NL__ \
          return plugin.EVENT_HANDLER(hook_args...);                       __NL__ \
       }                                                                   __NL__ \
@@ -118,58 +118,58 @@ _EVENT_HANDLER(onKeyswitchEvent, true)
 _EVENT_HANDLER(beforeReportingState, false)
 _EVENT_HANDLER(afterEachCycle, false)
 
-// The following _CALL macros are used by DEFINE_HOOKPOINT in
-// conjunction with the MAP macro to call the hook method in each listed plugin.
-
-#define _CALL_HOOK_FOR_PLUGIN(PLUGIN)                                       \
+// The following _CALL macro is used by _INLINE_EVENT_HANDLERS_FOR_HOOK in
+// conjunction with the MAP macro to call the event handler in each listed
+// plugin.
+#define _CALL_EVENT_HANDLER_FOR_PLUGIN(PLUGIN)                              \
    result = EventHandler__::call(PLUGIN, hook_args...);              __NL__ \
                                                                      __NL__ \
    if (EventHandler__::shouldAbortOnConsumedEvent() &&               __NL__ \
-       result == kaleidoscope::EventHandlerResult::EVENT_CONSUMED) {     __NL__ \
+       result == kaleidoscope::EventHandlerResult::EVENT_CONSUMED) { __NL__ \
       return result;                                                 __NL__ \
    }                                                                 __NL__
 
-#define _DEFINE_HOOKPOINT(CLASS_NAME, ...)                            \
-   struct CLASS_NAME                                                      __NL__ \
-   {                                                                      __NL__ \
-      /* Call the hook method on the plugin with the hook's arguments */  __NL__ \
-      template<typename EventHandler__, typename... Args__ >              __NL__ \
+#define _INLINE_EVENT_HANDLERS_FOR_HOOK(CLASS_NAME, ...)                             \
+   struct CLASS_NAME                                                          __NL__ \
+   {                                                                          __NL__ \
+      /* Call the event handler on the plugin with the handler's arguments */ __NL__ \
+      template<typename EventHandler__, typename... Args__ >                  __NL__ \
       static kaleidoscope::EventHandlerResult apply(Args__&&... hook_args) {  __NL__ \
          kaleidoscope::EventHandlerResult result;                             __NL__ \
-                                                                          __NL__ \
-         MAP(_CALL_HOOK_FOR_PLUGIN, __VA_ARGS__)                          __NL__ \
-                                                                          __NL__ \
-         return result;                                                   __NL__ \
-      }                                                                   __NL__ \
+                                                                              __NL__ \
+         MAP(_CALL_EVENT_HANDLER_FOR_PLUGIN, __VA_ARGS__)                     __NL__ \
+                                                                              __NL__ \
+         return result;                                                       __NL__ \
+      }                                                                       __NL__ \
    };
 
-#define _DEFINE_HOOKPOINT_METHOD(HOOK,SIGNATURE,...) \
-  EventHandlerResult Hooks::HOOK SIGNATURE {                               __NL__ \
-     return kaleidoscope_internal::HookPoint::template                 __NL__ \
+#define _DEFINE_EVENT_HANDLER(HOOK,SIGNATURE,...)                             \
+  EventHandlerResult Hooks::HOOK SIGNATURE {                           __NL__ \
+     return kaleidoscope_internal::EventHandlers::template             __NL__ \
      apply<kaleidoscope_internal::EventHandler_ ## HOOK>(__VA_ARGS__); __NL__ \
    }                                                                   __NL__
 
 /* KALEIDOSCOPE_INIT_PLUGINS should only be invoked in global namespace of the
    firmware sketch. It turns around and calls _KALEIDOSCOPE_INIT_PLUGINS here,
    which actually builds the loops that execute the plugins' implementations of
-   the various hooks.
+   the various event handlers.
 
    Its arguments are a list of references to plugin instances that have been
    instantiated in the global scope. */
 #define _KALEIDOSCOPE_INIT_PLUGINS(...)                                       \
    namespace kaleidoscope_internal {                                   __NL__ \
                                                                        __NL__ \
-   _DEFINE_HOOKPOINT(HookPoint, __VA_ARGS__)                           __NL__ \
+   _INLINE_EVENT_HANDLERS_FOR_HOOK(EventHandlers, __VA_ARGS__)         __NL__ \
                                                                        __NL__ \
    }                                                                   __NL__ \
                                                                        __NL__ \
    namespace kaleidoscope {                                            __NL__ \
-   _DEFINE_HOOKPOINT_METHOD(onSetup,())                                __NL__ \
-   _DEFINE_HOOKPOINT_METHOD(beforeEachCycle, ())                       __NL__ \
-   _DEFINE_HOOKPOINT_METHOD(onKeyswitchEvent,                          __NL__ \
+   _DEFINE_EVENT_HANDLER(onSetup,())                                   __NL__ \
+   _DEFINE_EVENT_HANDLER(beforeEachCycle, ())                          __NL__ \
+   _DEFINE_EVENT_HANDLER(onKeyswitchEvent,                             __NL__ \
                 (Key &mappedKey, byte row, byte col, uint8_t keyState),__NL__ \
                 mappedKey, row, col, keyState)                         __NL__ \
-   _DEFINE_HOOKPOINT_METHOD(beforeReportingState,())                   __NL__ \
-   _DEFINE_HOOKPOINT_METHOD(afterEachCycle,())                         __NL__ \
+   _DEFINE_EVENT_HANDLER(beforeReportingState,())                      __NL__ \
+   _DEFINE_EVENT_HANDLER(afterEachCycle,())                            __NL__ \
    }
 }
