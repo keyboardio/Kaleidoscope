7044060ebf074c7a6bf84f18e9158394bbd89a08
Gergely Nagy <algernon@keyboard.io>
2018-05-09 22:53:56 +0200

    Updated the README and the example to use the new plugin APIs
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- examples/LEDEffects/LEDEffects.ino
+++ examples/LEDEffects/LEDEffects.ino
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
  * Kaleidoscope-LEDEffects -- An assorted collection of LED effects for Kaleidoscope
- * Copyright (C) 2016, 2017  Gergely Nagy
+ * Copyright (C) 2016, 2017, 2018  Gergely Nagy
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@
 
 #include "LED-Off.h"
 
-
+// *INDENT-OFF*
 const Key keymaps[][ROWS][COLS] PROGMEM = {
   [0] = KEYMAP_STACKED
   (
@@ -35,17 +35,21 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
 
     Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
     Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
-    Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
+               Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
     Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
 
     Key_RightShift, Key_RightAlt, Key_Spacebar, Key_RightControl,
     Key_NoKey),
 };
+// *INDENT-ON*
 
+KALEIDOSCOPE_INIT_PLUGINS(LEDControl,
+                          LEDOff,
+                          MiamiEffect,
+                          JukeboxEffect,
+                          JukeboxAlternateEffect);
 
 void setup() {
-  Kaleidoscope.use(&LEDOff, &MiamiEffect, &JukeboxEffect, &JukeboxAlternateEffect);
-
   Kaleidoscope.setup();
 
   MiamiEffect.activate();
