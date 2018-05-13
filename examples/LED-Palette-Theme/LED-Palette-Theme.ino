5d71c259f44ce0d0fdfe7603f390c440aad80cc8
Gergely Nagy <algernon@keyboard.io>
2018-05-13 10:02:48 +0200

    Updated to use the new plugin APIs
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- examples/LED-Palette-Theme/LED-Palette-Theme.ino
+++ examples/LED-Palette-Theme/LED-Palette-Theme.ino
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
  * Kaleidoscope-LED-Palette-Theme -- Palette-based LED theme foundation
- * Copyright (C) 2017  Gergely Nagy
+ * Copyright (C) 2017, 2018  Gergely Nagy
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -59,6 +59,7 @@ TestLEDMode::focusHook(const char *command) {
 
 example::TestLEDMode TestLEDMode;
 
+// *INDENT-OFF*
 const Key keymaps[][ROWS][COLS] PROGMEM = {
   [0] = KEYMAP_STACKED
   (
@@ -72,17 +73,18 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
 
     Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
     Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
-    Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
+               Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
     Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
 
     Key_RightShift, Key_RightAlt, Key_Spacebar, Key_RightControl,
     Key_NoKey),
 };
+// *INDENT-ON*
+
+KALEIDOSCOPE_INIT_PLUGINS(Focus, LEDPaletteTheme, TestLEDMode, EEPROMSettings);
 
 void setup() {
   Serial.begin(9600);
-
-  Kaleidoscope.use(&Focus, &LEDPaletteTheme, &TestLEDMode, &EEPROMSettings);
 
   Kaleidoscope.setup();
 
