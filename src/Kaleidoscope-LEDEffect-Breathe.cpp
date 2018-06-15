9592ef1ae85cc50337bfb06fc3db8e1606b6938c
Gergely Nagy <algernon@keyboard.io>
2018-06-15 15:26:44 +0200

    Make the hue configurable
    Fixes #3.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/Kaleidoscope-LEDEffect-Breathe.cpp
+++ src/Kaleidoscope-LEDEffect-Breathe.cpp
@@ -2,7 +2,7 @@
 
 namespace kaleidoscope {
 void LEDBreatheEffect::update(void) {
-  cRGB color = breath_compute();
+  cRGB color = breath_compute(hue);
   ::LEDControl.set_all_leds_to(color);
 }
 }
--- src/Kaleidoscope-LEDEffect-Breathe.h
+++ src/Kaleidoscope-LEDEffect-Breathe.h
@@ -8,6 +8,8 @@ class LEDBreatheEffect : public LEDMode {
  public:
   LEDBreatheEffect(void) {}
 
+  uint8_t hue = 170;
+
  protected:
   void update(void) final;
 };
