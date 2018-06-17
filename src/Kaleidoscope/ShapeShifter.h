c489f6702f5d5515ba3678c98f03d9adee37651a
Gergely Nagy <algernon@keyboard.io>
2018-06-17 11:22:45 +0200

    Override onSetup when using a compatibility layer
    The default `onSetup` will call `.begin`, to support initializing plugins using
    the V1 plugins while using `KALEIDOSCOPE_INIT_PLUGINS`. However, plugins that
    implement a compatibility layer so that they can be used with both the new API,
    and with `Kaleidoscope.use()` will have a `.begin` method too. Which the default
    `onSetup` will call, and we'll register the compatibility layer too, in addition
    to the new-style event handlers. This results in many things running twice,
    which leads to all kinds of problems.
    
    For this reason, override `onSetup`, so that it does not call `begin`. When used
    with `Kaleidoscope.use()`, the plugin will still work, so compatibility is
    maintained. But the bug is now gone.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/Kaleidoscope/ShapeShifter.h
+++ src/Kaleidoscope/ShapeShifter.h
@@ -36,6 +36,10 @@ class ShapeShifter : public KaleidoscopePlugin {
   EventHandlerResult beforeReportingState();
 
 #if KALEIDOSCOPE_ENABLE_V1_PLUGIN_API
+  kaleidoscope::EventHandlerResult onSetup() {
+    return kaleidoscope::EventHandlerResult::OK;
+  }
+
  protected:
   void begin();
   static Key legacyEventHandler(Key mapped_key, byte row, byte col, uint8_t key_state);
