f7b9c65d347cd8bb749d64f6c87a684a01458e05
Gergely Nagy <algernon@keyboard.io>
2018-08-22 12:16:28 +0200

    Assign my copyright to Keyboard.io
    While the original plugin was written independently, significant developments
    were made while working for Keyboard.io. As such, I feel it is appropriate to
    assign copyright to the company.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/Kaleidoscope/Jukebox.cpp
+++ src/Kaleidoscope/Jukebox.cpp
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
  * Kaleidoscope-LEDEffects -- An assorted collection of LED effects for Kaleidoscope
- * Copyright (C) 2016  Gergely Nagy
+ * Copyright (C) 2016  Keyboard.io, Inc
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
