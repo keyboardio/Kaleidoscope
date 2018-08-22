154a600c678c8fcf8a9f554644bba8fa644f4260
Gergely Nagy <algernon@keyboard.io>
2018-08-22 12:17:39 +0200

    Assign my copyright to Keyboard.io
    While the original plugin was written independently, significant developments
    were made while working for Keyboard.io. As such, I feel it is appropriate to
    assign copyright to the company.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/Kaleidoscope-Syster.h
+++ src/Kaleidoscope-Syster.h
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
  * Kaleidoscope-Syster -- Symbolic input system
- * Copyright (C) 2016, 2017  Gergely Nagy
+ * Copyright (C) 2016, 2017  Keyboard.io, Inc
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
--- src/Kaleidoscope/Syster.cpp
+++ src/Kaleidoscope/Syster.cpp
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
  * Kaleidoscope-Syster -- Symbolic input system
- * Copyright (C) 2017, 2018  Gergely Nagy
+ * Copyright (C) 2017, 2018  Keyboard.io, Inc
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
