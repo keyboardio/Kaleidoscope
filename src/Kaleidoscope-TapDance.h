fc9244649a38dbf692db6c79e8b002388a6c0039
Gergely Nagy <algernon@keyboard.io>
2018-08-22 12:17:51 +0200

    Assign my copyright to Keyboard.io
    While the original plugin was written independently, significant developments
    were made while working for Keyboard.io. As such, I feel it is appropriate to
    assign copyright to the company.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/Kaleidoscope-TapDance.h
+++ src/Kaleidoscope-TapDance.h
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
  * Kaleidoscope-TapDance -- Tap-dance keys
- * Copyright (C) 2016, 2017  Gergely Nagy
+ * Copyright (C) 2016, 2017  Keyboard.io, Inc
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
