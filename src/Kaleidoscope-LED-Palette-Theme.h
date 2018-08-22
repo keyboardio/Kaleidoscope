1b6bbee1a036ba49f93f2cbbb0c85aa71cec34d7
Gergely Nagy <algernon@keyboard.io>
2018-08-22 12:16:39 +0200

    Assign my copyright to Keyboard.io
    While the original plugin was written independently, significant developments
    were made while working for Keyboard.io. As such, I feel it is appropriate to
    assign copyright to the company.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/Kaleidoscope-LED-Palette-Theme.h
+++ src/Kaleidoscope-LED-Palette-Theme.h
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
  * Kaleidoscope-LED-Palette-Theme -- Palette-based LED theme foundation
- * Copyright (C) 2017  Gergely Nagy
+ * Copyright (C) 2017  Keyboard.io, Inc
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
