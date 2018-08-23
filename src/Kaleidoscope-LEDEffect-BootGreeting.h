9d4794910911aa90c5db858c6e31dca2c8e00f27
Gergely Nagy <algernon@keyboard.io>
2018-08-23 21:54:33 +0200

    Assign my copyright to Keyboard.io
    While the original plugin was written independently, significant developments
    were made while working for Keyboard.io. As such, I feel it is appropriate to
    assign copyright to the company.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/Kaleidoscope-LEDEffect-BootGreeting.h
+++ src/Kaleidoscope-LEDEffect-BootGreeting.h
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
  * Kaleidoscope-LEDEffect-BootGreeting -- Small greeting at boot time
- * Copyright (C) 2017, 2018  Gergely Nagy
+ * Copyright (C) 2017, 2018  Keyboard.io, Inc
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
