3bc00e173c3562456297ef1fd333356750c848f0
Gergely Nagy <algernon@keyboard.io>
2018-10-25 08:54:13 +0200

    New plugin: IdleLEDs
    Suggested by `nev` on Discord, this plugin will turn the LEDs off after a
    configurable idle time, and back on on the next keypress.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ src/kaleidoscope/plugin/IdleLEDs.cpp
@@ -0,0 +1,56 @@
+/* -*- mode: c++ -*-
+ * Kaleidoscope-Idle-LEDs -- Turn off the LEDs when the keyboard's idle
+ * Copyright (C) 2018  Keyboard.io, Inc
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Kaleidoscope-IdleLEDs.h>
+#include <Kaleidoscope-LEDControl.h>
+
+namespace kaleidoscope {
+namespace plugin {
+
+uint16_t IdleLEDs::idle_time_limit = 600; // 10 minutes
+uint32_t IdleLEDs::last_keypress_time_;
+
+EventHandlerResult IdleLEDs::beforeEachCycle() {
+  uint32_t idle_limit = idle_time_limit * 1000;
+
+  if (!::LEDControl.paused &&
+      Kaleidoscope.millisAtCycleStart() - last_keypress_time_ >= idle_limit) {
+    ::LEDControl.set_all_leds_to(CRGB(0, 0, 0));
+    ::LEDControl.syncLeds();
+
+    ::LEDControl.paused = true;
+  }
+
+  return EventHandlerResult::OK;
+}
+
+EventHandlerResult IdleLEDs::onKeyswitchEvent(Key &mapped_key, byte row, byte col, uint8_t key_state) {
+
+  if (::LEDControl.paused) {
+    ::LEDControl.paused = false;
+    ::LEDControl.refreshAll();
+  }
+
+  last_keypress_time_ = Kaleidoscope.millisAtCycleStart();
+
+  return EventHandlerResult::OK;
+}
+
+}
+}
+
+kaleidoscope::plugin::IdleLEDs IdleLEDs;
