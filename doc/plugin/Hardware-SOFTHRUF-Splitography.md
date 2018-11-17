f527591b57f5700326479949e295e72a28cab028
Gergely Nagy <algernon@keyboard.io>
2018-11-17 10:04:27 +0100

    New hardware: SOFT/HRUF Splitography
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ doc/plugin/Hardware-SOFTHRUF-Splitography.md
@@ -0,0 +1,5 @@
+# Kaleidoscope-Hardware-SOFTHRUF-Splitography
+
+Hardware support for the [SOFTHRUF Splitography][splitography] Steno keyboard.
+
+ [splitography]: https://softhruf.love/collections/writers
---
+++ src/kaleidoscope/hardware/softhruf/Splitography.cpp
@@ -0,0 +1,44 @@
+/* -*- mode: c++ -*-
+ * Kaleidoscope-Hardware-SOFTHRUF-Splitography -- Splitography hardware support for Kaleidoscope
+ * Copyright (C) 2018  Keyboard.io, Inc
+ *
+ * Based on QMK (commit e9a67f8fd) and sdothum's fork (commit 8616b44)
+ *  (C) Jack Humbert, Jun Wako, Steven Hum, and others
+ * Original QMK sources:
+ *  - keyboards/splitography/config.h
+ *  - keyboards/splitography/splitography.h
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef ARDUINO_AVR_SPLITOGRAPHY
+
+#include <Kaleidoscope.h>
+#include <avr/wdt.h>
+
+namespace kaleidoscope {
+namespace hardware {
+namespace softhruf {
+
+ATMEGA_KEYBOARD_DATA(Splitography);
+constexpr int8_t Splitography::led_count;
+
+}
+}
+}
+
+HARDWARE_IMPLEMENTATION KeyboardHardware;
+kaleidoscope::hardware::softhruf::Splitography &Splitography = KeyboardHardware;
+
+#endif
