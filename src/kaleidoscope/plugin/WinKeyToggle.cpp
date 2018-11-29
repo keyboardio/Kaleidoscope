de2dc3daf68530732f2be5862770a71a9a72bf38
Gergely Nagy <algernon@keyboard.io>
2018-11-29 08:51:50 +0100

    New plugin: WinKeyToggle
    Based on a discussion on Discord, this implements a simple plugin that can
    temporarily disable the Windows (GUI) keys.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ src/kaleidoscope/plugin/WinKeyToggle.cpp
@@ -0,0 +1,38 @@
+/* -*- mode: c++ -*-
+ * Kaleidoscope-WinKeyToggle -- Toggle the Windows (GUI) key on/off
+ * Copyright (C) 2018  Keyboard.io, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Kaleidoscope.h>
+#include <Kaleidoscope-WinKeyToggle.h>
+
+namespace kaleidoscope {
+namespace plugin {
+
+bool WinKeyToggle::enabled_;
+
+EventHandlerResult WinKeyToggle::onKeyswitchEvent(Key &key, byte row, byte col, uint8_t key_state) {
+  if (!enabled_)
+    return EventHandlerResult::OK;
+
+  if (key == Key_LeftGui || key == Key_RightGui)
+    return EventHandlerResult::EVENT_CONSUMED;
+
+  return EventHandlerResult::OK;
+}
+}
+}
+
+kaleidoscope::plugin::WinKeyToggle WinKeyToggle;
---
+++ src/kaleidoscope/plugin/WinKeyToggle.h
@@ -0,0 +1,38 @@
+/* -*- mode: c++ -*-
+ * Kaleidoscope-WinKeyToggle -- Toggle the Windows (GUI) key on/off
+ * Copyright (C) 2018  Keyboard.io, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <Kaleidoscope.h>
+
+namespace kaleidoscope {
+namespace plugin {
+class WinKeyToggle: public kaleidoscope::Plugin {
+ public:
+  WinKeyToggle() {}
+
+  EventHandlerResult onKeyswitchEvent(Key &key, byte row, byte col, uint8_t key_state);
+  void toggle() {
+    enabled_ = !enabled_;
+  }
+ private:
+  static bool enabled_;
+};
+}
+}
+
+extern kaleidoscope::plugin::WinKeyToggle WinKeyToggle;
