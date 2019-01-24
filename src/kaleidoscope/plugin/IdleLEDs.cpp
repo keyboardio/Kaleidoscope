7e630e17858eb538e48c403f284b50470d43d1a5
Gergely Nagy <algernon@keyboard.io>
2019-01-24 10:15:15 +0100

    IdleLEDs: Add support for Underglow too
    If we have Underglow LEDs, turn them off when idle, too.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/kaleidoscope/plugin/IdleLEDs.cpp
+++ src/kaleidoscope/plugin/IdleLEDs.cpp
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
  * Kaleidoscope-Idle-LEDs -- Turn off the LEDs when the keyboard's idle
- * Copyright (C) 2018  Keyboard.io, Inc
+ * Copyright (C) 2018-2019  Keyboard.io, Inc
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -17,6 +17,7 @@
 
 #include <Kaleidoscope-IdleLEDs.h>
 #include <Kaleidoscope-LEDControl.h>
+#include <Kaleidoscope-UnderglowControl.h>
 
 namespace kaleidoscope {
 namespace plugin {
@@ -25,12 +26,19 @@ uint16_t IdleLEDs::idle_time_limit = 600; // 10 minutes
 uint32_t IdleLEDs::end_time_;
 
 EventHandlerResult IdleLEDs::beforeEachCycle() {
-  if (!::LEDControl.paused &&
-      (Kaleidoscope.millisAtCycleStart() >= end_time_)) {
+  if (Kaleidoscope.millisAtCycleStart() < end_time_)
+    return EventHandlerResult::OK;
+
+  if (::Kaleidoscope.has_leds && !::LEDControl.paused) {
     ::LEDControl.set_all_leds_to(CRGB(0, 0, 0));
     ::LEDControl.syncLeds();
 
     ::LEDControl.paused = true;
+  }
+  if (::UnderglowControl.has_leds() && !::UnderglowControl.isPaused()) {
+    ::UnderglowControl.setColor(0, 0, 0);
+    ::UnderglowControl.sync();
+    ::UnderglowControl.pause();
   }
 
   return EventHandlerResult::OK;
@@ -38,9 +46,12 @@ EventHandlerResult IdleLEDs::beforeEachCycle() {
 
 EventHandlerResult IdleLEDs::onKeyswitchEvent(Key &mapped_key, byte row, byte col, uint8_t key_state) {
 
-  if (::LEDControl.paused) {
+  if (Kaleidoscope.has_leds && ::LEDControl.paused) {
     ::LEDControl.paused = false;
     ::LEDControl.refreshAll();
+  }
+  if (::UnderglowControl.has_leds() && ::UnderglowControl.isPaused()) {
+    ::UnderglowControl.resume();
   }
 
   end_time_ = Kaleidoscope.millisAtCycleStart() + (uint32_t)idle_time_limit * 1000;
--- src/kaleidoscope/plugin/IdleLEDs.h
+++ src/kaleidoscope/plugin/IdleLEDs.h
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
  * Kaleidoscope-Idle-LEDs -- Turn off the LEDs when the keyboard's idle
- * Copyright (C) 2018  Keyboard.io, Inc
+ * Copyright (C) 2018-2019  Keyboard.io, Inc
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
