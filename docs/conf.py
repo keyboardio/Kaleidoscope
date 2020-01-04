2ea080d6e545ae3a94dd240d1a9d4b544df8e9a4
Jesse Vincent <jesse@keyboard.io>
2020-01-03 17:25:51 -0800

    try to get rtd to see our master doc

--- docs/conf.py
+++ docs/conf.py
@@ -97,3 +97,6 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+master_doc = 'index.rst'
