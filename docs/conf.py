77d6ee8118a9d551a3dcf52f86c1976ce4d2f326
Jesse Vincent <jesse@keyboard.io>
2020-01-03 17:17:41 -0800

    Another attempt to make rtd happy

--- docs/conf.py
+++ docs/conf.py
@@ -93,9 +93,6 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-if on_rtd:
-    master_doc = '../../docs/index'
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
