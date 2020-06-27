90b086137c816fb72ef3a5fb2ee5c424d96bb006
Jesse Vincent <jesse@keyboard.io>
2020-06-26 19:12:00 -0700

    Remove use of ATMEGA32U4_KEYBOARD macro for the Atreus
    Trying to get back to code rather than macros

--- src/kaleidoscope/device/keyboardio/Atreus2.h
+++ src/kaleidoscope/device/keyboardio/Atreus2.h
@@ -29,11 +29,24 @@ namespace kaleidoscope {
 namespace device {
 namespace keyboardio {
 
-ATMEGA32U4_KEYBOARD(
-  Atreus, Caterina, "atreus",
-  ROW_PIN_LIST({PIN_F6, PIN_F5, PIN_F4, PIN_F1}),
-  COL_PIN_LIST({PIN_F7, PIN_E2, PIN_C7, PIN_C6, PIN_B6, PIN_B5, PIN_D7, PIN_D6, PIN_D4, PIN_D5, PIN_D3, PIN_D2})
-);
+
+  ATMEGA32U4_DEVICE_PROPS(Atreus, Caterina, "atreus",                          
+                          ROW_PIN_LIST({PIN_F6, PIN_F5, PIN_F4, PIN_F1}),
+                          COL_PIN_LIST({PIN_F7, PIN_E2, PIN_C7, PIN_C6, PIN_B6, PIN_B5, PIN_D7, PIN_D6, PIN_D4, PIN_D5, PIN_D3, PIN_D2}));
+
+#ifndef KALEIDOSCOPE_VIRTUAL_BUILD
+  ATMEGA32U4_DEVICE(Atreus)
+
+#else // ifndef KALEIDOSCOPE_VIRTUAL_BUILD
+  /* Device definition omitted for virtual device builds.                      
+   * We need to forward declare the device name, though, as there are          
+   * some legacy extern references to boards whose definition                  
+   * depends on this.                                                          
+   */                                                                          
+  class Atreus;
+
+#endif // ifndef KALEIDOSCOPE_VIRTUAL_BUILD
+
 
 #define PER_KEY_DATA(dflt,                                                    \
     R0C0, R0C1, R0C2, R0C3, R0C4,             R0C7, R0C8, R0C9, R0C10, R0C11, \
