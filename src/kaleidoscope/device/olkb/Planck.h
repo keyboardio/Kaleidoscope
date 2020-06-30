2ae8f31e7dc46cece76210ddc9a2344840327aa3
Gergely Nagy <algernon@keyboard.io>
2020-06-30 11:35:35 +0200

    olkb/Planck: Stop using opaque macros
    Instead of using opaque macros which aren't even extensible, just expand them,
    and use the raw data structures for the hardware definition. While this is more
    verbose than the macros, it is more future proof, and clearer for the reader
    too, because they don't need to understand the magic macros.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/kaleidoscope/device/olkb/Planck.h
+++ src/kaleidoscope/device/olkb/Planck.h
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
  * Kaleidoscope-Hardware-OLKB-Planck -- Planck hardware support for Kaleidoscope
- * Copyright (C) 2018, 2019  Keyboard.io, Inc
+ * Copyright (C) 2018, 2019, 2020  Keyboard.io, Inc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of version 3 of the GNU General Public License as
@@ -28,11 +28,32 @@ namespace kaleidoscope {
 namespace device {
 namespace olkb {
 
-ATMEGA32U4_KEYBOARD(
-  Planck, HalfKay, "planck",
-  ROW_PIN_LIST({ PIN_D0, PIN_D5, PIN_B5, PIN_B6 }),
-  COL_PIN_LIST({ PIN_F1, PIN_F0, PIN_B0, PIN_C7, PIN_F4, PIN_F5, PIN_F6, PIN_F7, PIN_D4, PIN_D6, PIN_B4, PIN_D7 })
-);
+struct PlanckProps : kaleidoscope::device::ATmega32U4KeyboardProps {
+  struct KeyScannerProps : public kaleidoscope::driver::keyscanner::ATmegaProps {
+    static constexpr uint8_t matrix_rows = 4;
+    static constexpr uint8_t matrix_columns = 12;
+    typedef MatrixAddr<matrix_rows, matrix_columns> KeyAddr;
+#ifndef KALEIDOSCOPE_VIRTUAL_BUILD
+    static constexpr uint8_t matrix_row_pins[matrix_rows] = {PIN_D0, PIN_D5, PIN_B5, PIN_B6};
+    static constexpr uint8_t matrix_col_pins[matrix_columns] = {PIN_F0, PIN_F1, PIN_F4, PIN_F5, PIN_F6, PIN_F7, PIN_B3, PIN_B1, PIN_B0, PIN_D5, PIN_B7, PIN_C7};
+#endif // KALEIDOSCOPE_VIRTUAL_BUILD
+  };
+  typedef kaleidoscope::driver::keyscanner::ATmega<KeyScannerProps> KeyScanner;
+  typedef kaleidoscope::driver::bootloader::avr::HalfKay Bootloader;
+  static constexpr const char *short_name = "planck";
+};
+
+#ifndef KALEIDOSCOPE_VIRTUAL_BUILD
+class Planck: public kaleidoscope::device::ATmega32U4Keyboard<PlanckProps> {};
+#else // ifndef KALEIDOSCOPE_VIRTUAL_BUILD
+/* Device definition omitted for virtual device builds.
+ * We need to forward declare the device name, though, as there are
+ * some legacy extern references to boards whose definition
+ * depends on this.
+ */
+class Planck;
+
+#endif // ifndef KALEIDOSCOPE_VIRTUAL_BUILD
 
 #define PER_KEY_DATA(dflt,                                                       \
          R0C0, R0C1, R0C2, R0C3, R0C4, R0C5, R0C6, R0C7, R0C8, R0C9, R0C10, R0C11, \
