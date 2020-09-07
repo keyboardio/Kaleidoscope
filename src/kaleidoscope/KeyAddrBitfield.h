8477e5369c5b6c26b659b62b16d47c7f387f976b
Michael Richters <gedankenexperimenter@gmail.com>
2020-09-07 13:18:42 -0500

    Add copyright notice to KeyAddrBitfield.h
    Signed-off-by: Michael Richters <gedankenexperimenter@gmail.com>

--- src/kaleidoscope/KeyAddrBitfield.h
+++ src/kaleidoscope/KeyAddrBitfield.h
@@ -1,4 +1,18 @@
-// -*- c++ -*-
+/* Kaleidoscope - Firmware for computer input devices
+ * Copyright (C) 2020  Keyboard.io, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
