bc7bb0c98b7611c4f45fe7cb3f8f79530d85788a
Michael Richters <gedankenexperimenter@gmail.com>
2020-09-07 13:21:17 -0500

    Update copyright dates for ActiveModColor
    Signed-off-by: Michael Richters <gedankenexperimenter@gmail.com>

--- src/kaleidoscope/plugin/LED-ActiveModColor.h
+++ src/kaleidoscope/plugin/LED-ActiveModColor.h
@@ -1,6 +1,6 @@
 /* -*- mode: c++ -*-
  * Kaleidoscope-LED-ActiveModColor -- Light up the LEDs under the active modifiers
- * Copyright (C) 2016, 2017, 2018  Keyboard.io, Inc
+ * Copyright (C) 2016-2020  Keyboard.io, Inc
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
