ad5031f64a2e85ce7cf733af1abd2d1871a83cbe
Michael Richters <gedankenexperimenter@gmail.com>
2020-10-03 14:34:34 -0700

    Add a new SYSTEM_KEY definition helper macro to match CONSUMER_KEY

--- src/kaleidoscope/key_defs.h
+++ src/kaleidoscope/key_defs.h
@@ -255,6 +255,8 @@ typedef kaleidoscope::Key Key_;
 #define KEY_LEFT_FN2 0xff
 #define Key_LFN2 Key(KEY_LEFT_FN2, KEY_FLAGS)
 
+#define SYSTEM_KEY(code, hid_type) \
+  Key(code, SYNTHETIC | IS_SYSCTL | (hid_type & HID_TYPE_MASK))
 
 /* Most Consumer keys are more then 8bit, the highest Consumer hid code
    uses 10bit. By using the 11bit as flag to indicate a consumer keys was activate we can
