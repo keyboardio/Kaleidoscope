57c41719a7b0566bccbaf1caf4226afe07344a31
Jesse Vincent <jesse@keyboard.io>
2020-10-03 15:40:37 -0700

    Add a test for a keycode that will only work with the new HID Usage Tables

---
+++ tests/hid/hid-v1.2-consumer-keys/test/testcase.cpp
@@ -0,0 +1,54 @@
+/* -*- mode: c++ -*-
+ * Copyright (C) 2020  Eric Paniagua (epaniagua@google.com)
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "testing/setup-googletest.h"
+
+SETUP_GOOGLETEST();
+
+namespace kaleidoscope {
+namespace testing {
+namespace {
+
+using ::testing::IsEmpty;
+
+class KeyboardReports : public VirtualDeviceTest {};
+
+TEST_F(KeyboardReports, HIDUsageTablev12KeycodesAdded) {
+  sim_.Press(1, 1); // VoiceCommand
+  auto state = RunCycle();
+
+  ASSERT_EQ(state->HIDReports()->ConsumerControl().size(), 1);
+  EXPECT_THAT(
+    state->HIDReports()->ConsumerControl(0).Keys(),
+    Contains(Consumer_VoiceCommand));
+
+  sim_.Release(1, 1);  // VoiceCommand
+  state = RunCycle();
+
+  ASSERT_EQ(state->HIDReports()->ConsumerControl().size(), 1);
+  EXPECT_THAT(
+    state->HIDReports()->ConsumerControl(0).Keys(),
+    IsEmpty());
+
+  state = RunCycle();
+
+  // 2 cycles after releasing the key
+  EXPECT_EQ(state->HIDReports()->ConsumerControl().size(), 0);
+}
+
+}  // namespace
+}  // namespace testing
+}  // namespace kaleidoscope
