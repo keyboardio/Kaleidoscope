dd9faf910c4cc307082ca757813e117f22595865
Jesse Vincent <jesse@keyboard.io>
2020-10-05 23:30:21 -0700

    Rework our travis config to be a bit more efficient, moving the work dir to a ramdisk, checking less stuff out of git, etc.

---
+++ .travis/setup-ramdisk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+export BLDDIR=/home/travis/build
+
+df -h
+du -sh $HOME
+du -sh $BLDDIR
+sudo mv $BLDDIR $BLDDIR.ori
+sudo mkdir -p $BLDDIR
+sudo mount -t tmpfs -o size=8192m tmps $BLDDIR
+time sudo cp -R $BLDDIR.ori/. $BLDDIR
+sudo chown -R travis:travis $BLDDIR
+df -h
+du -sh "$HOME"
+du -sh "$BLDDIR"
