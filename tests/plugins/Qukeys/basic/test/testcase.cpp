fabcb0a89ff3c159b1c766fceee79fe152fd78aa
Michael Richters <gedankenexperimenter@gmail.com>
2020-10-29 23:58:47 -0500

    More testcases converted

--- tests/plugins/Qukeys/basic/test/testcase.cpp
+++ tests/plugins/Qukeys/basic/test/testcase.cpp
@@ -70,17 +70,31 @@ class QukeysBasic : public VirtualDeviceTest {
   std::set<uint8_t> current_keyboard_keycodes_ = {};
   std::vector<ExpectedKeyboardReport> expected_reports_ = {};
 
+  std::vector<uint32_t> input_timestamps_ = {};
+
+  std::unique_ptr<State> output_state_ = nullptr;
+
   std::set<uint8_t> expected_keycodes_ = {};
   std::unique_ptr<State> state_ = nullptr;
+
+  uint32_t reportTimestamp(size_t index) {
+    return output_state_->HIDReports()->Keyboard(index).Timestamp();
+  }
+
+  uint32_t keyswitchTimestamp(size_t index) {
+    return input_timestamps_[index];
+  }
 
   uint32_t pressKey(uint32_t delay, KeyAddr addr) {
     sim_.RunForMillis(delay);
     sim_.Press(addr);
+    input_timestamps_.push_back(Runtime.millisAtCycleStart());
     return Runtime.millisAtCycleStart();
   }
   uint32_t releaseKey(uint32_t delay, KeyAddr addr) {
     sim_.RunForMillis(delay);
     sim_.Release(addr);
+    input_timestamps_.push_back(Runtime.millisAtCycleStart());
     return Runtime.millisAtCycleStart();
   }
 
@@ -107,10 +121,10 @@ class QukeysBasic : public VirtualDeviceTest {
     return report_timestamp;
   }
   uint32_t expectReportWith(Key key, std::string message = "") {
-    return expectReport(std::set<Key>{key}, std::set<Key>{}, message);
+    return expectReport(std::set<Key> {key}, std::set<Key> {}, message);
   }
   uint32_t expectReportWithout(Key key, std::string message = "") {
-    return expectReport(std::set<Key>{}, std::set<Key>{key}, message);
+    return expectReport(std::set<Key> {}, std::set<Key> {key}, message);
   }
   uint32_t expectReportAfterCycles(size_t n,
                                    std::set<Key> added_keys,
@@ -120,7 +134,7 @@ class QukeysBasic : public VirtualDeviceTest {
     return expectReport(added_keys, removed_keys, message);
   }
   uint32_t expectReportAfterCycles(size_t n, std::string message = "") {
-    return expectReportAfterCycles(n, std::set<Key>{}, std::set<Key>{},
+    return expectReportAfterCycles(n, std::set<Key> {}, std::set<Key> {},
                                    message);
   }
   uint32_t expectReportAfterCyclesWith(size_t n, Key key,
@@ -141,7 +155,7 @@ class QukeysBasic : public VirtualDeviceTest {
     return expectReport(added_keys, removed_keys, message);
   }
   uint32_t expectReportAfterMillis(uint32_t t, std::string message = "") {
-    return expectReportAfterMillis(t, std::set<Key>{}, std::set<Key>{},
+    return expectReportAfterMillis(t, std::set<Key> {}, std::set<Key> {},
                                    message);
   }
   uint32_t expectReportAfterMillisWith(uint32_t t, Key key,
@@ -161,9 +175,9 @@ TEST_F(QukeysBasic, TapQukeyAlone) {
   pressKey(10, key_addr_A);
   releaseKey(50, key_addr_A);
   expectReportAfterCyclesWith(
-      1, Key_A, "Report should contain only `A`");
+    1, Key_A, "Report should contain only `A`");
   expectReportAfterCyclesWithout(
-      1, Key_A, "Report should be empty");
+    1, Key_A, "Report should be empty");
 
   sim_.RunForMillis(10);
   auto state = RunCycle();
@@ -181,31 +195,38 @@ TEST_F(QukeysBasic, TapQukeyAlone) {
 
 TEST_F(QukeysBasic, HoldQukeyAlone) {
   constexpr uint32_t hold_time = 400;
-  
-  uint32_t t0 = pressKey(10, key_addr_A);
-  uint32_t t1 = expectReportAfterMillisWith(QUKEYS_HOLD_TIMEOUT, Key_LeftGui);
-  uint32_t t2 = releaseKey(hold_time, key_addr_A);
-  uint32_t t3 = expectReportAfterCyclesWithout(1, Key_LeftGui);
+
+  pressKey(10, key_addr_A);
+  expectReportAfterMillisWith(
+    QUKEYS_HOLD_TIMEOUT, Key_LeftGui,
+    "The first report should include only `LeftGui`");
+  releaseKey(hold_time, key_addr_A);
+  expectReportAfterCyclesWithout(
+    1, Key_LeftGui,
+    "The second report should be empty");
 
   sim_.RunForMillis(10);
-  auto state = RunCycle();
+  output_state_ = RunCycle();
 
-  ASSERT_EQ(state->HIDReports()->Keyboard().size(), 2)
+  constexpr int expected_report_count = 2;
+  ASSERT_EQ(output_state_->HIDReports()->Keyboard().size(), expected_report_count)
       << "There should be two HID reports after a qukey is held and released";
 
-  uint32_t measured_delay = state->HIDReports()->Keyboard(0).Timestamp() - t0;
+  uint32_t measured_delay = reportTimestamp(0) - input_timestamps_[0];
   EXPECT_THAT(measured_delay, ::testing::Ge(QUKEYS_HOLD_TIMEOUT))
       << "The HID report should be sent after the hold timeout has elapsed";
-  EXPECT_THAT(state->HIDReports()->Keyboard(0).ActiveKeycodes(),
-              ::testing::ElementsAreArray(expected_reports_[0].Keycodes()))
-      << "The first report should include only `LeftGui`";
 
-  measured_delay = state->HIDReports()->Keyboard(1).Timestamp() - t1;
-  EXPECT_THAT(measured_delay, ::testing::Ge(hold_time))
+  // std::cerr << keyswitchTimestamp(1) << std::endl;
+  // std::cerr << reportTimestamp(1) << std::endl;
+
+  EXPECT_THAT(reportTimestamp(1), ::testing::Ge(keyswitchTimestamp(1)))
       << "The key should stay active until release";
-  EXPECT_THAT(state->HIDReports()->Keyboard(1).ActiveKeycodes(),
-              ::testing::ElementsAreArray(expected_reports_[1].Keycodes()))
-      << "The second report should be empty";
+
+  for (int i = 0; i < expected_report_count; ++i) {
+    EXPECT_THAT(output_state_->HIDReports()->Keyboard(i).ActiveKeycodes(),
+                ::testing::ElementsAreArray(expected_reports_[i].Keycodes()))
+        << expected_reports_[i].Message();
+  }
 }
 
 TEST_F(QukeysBasic, FullOverlap) {
@@ -213,33 +234,32 @@ TEST_F(QukeysBasic, FullOverlap) {
   pressKey(10, key_addr_F);
   pressKey(20, key_addr_X);
   releaseKey(50, key_addr_X);
-  addToReport(Key_LeftShift);
-  expectReportAfterCycles(1);
-  addToReport(Key_X);
-  expectReportAfterCycles(1);
-  removeFromReport(Key_X);
-  expectReportAfterCycles(1);
+  expectReportAfterCyclesWith(
+    1, Key_LeftShift,
+    "The first report should contain only `LeftShift`");
+  expectReportAfterCyclesWith(
+    1, Key_X,
+    "The second report should add `X`");
+  expectReportAfterCyclesWithout(
+    1, Key_X,
+    "The third report should remove `X`");
   releaseKey(10, key_addr_F);
-  removeFromReport(Key_LeftShift);
-  expectReportAfterCycles(1);
+  expectReportAfterCyclesWithout(
+    1, Key_LeftShift,
+    "The fourth report should remove `LeftShift`");
 
   sim_.RunForMillis(10);
-  auto state = RunCycle();
+  output_state_ = RunCycle();
 
-  ASSERT_EQ(state->HIDReports()->Keyboard().size(), 4)
+  constexpr int expected_report_count = 4;
+  ASSERT_EQ(output_state_->HIDReports()->Keyboard().size(), expected_report_count)
       << "There should be four HID reports total";
-  EXPECT_THAT(state->HIDReports()->Keyboard(0).ActiveKeycodes(),
-              ::testing::ElementsAreArray(expected_reports_[0].Keycodes()))
-      << "The first report should contain only `LeftShift`";
-  EXPECT_THAT(state->HIDReports()->Keyboard(1).ActiveKeycodes(),
-              ::testing::ElementsAreArray(expected_reports_[1].Keycodes()))
-      << "The second report should add `X`";
-  EXPECT_THAT(state->HIDReports()->Keyboard(2).ActiveKeycodes(),
-              ::testing::ElementsAreArray(expected_reports_[2].Keycodes()))
-      << "The third report should remove `X`";
-  EXPECT_THAT(state->HIDReports()->Keyboard(3).ActiveKeycodes(),
-              ::testing::ElementsAreArray(expected_reports_[3].Keycodes()))
-      << "The fourth report should remove `LeftShift`";
+
+  for (int i = 0; i < expected_report_count; ++i) {
+    EXPECT_THAT(output_state_->HIDReports()->Keyboard(i).ActiveKeycodes(),
+                ::testing::ElementsAreArray(expected_reports_[i].Keycodes()))
+        << expected_reports_[i].Message();
+  }
 }
 
 TEST_F(QukeysBasic, RolloverPrimary) {
@@ -250,9 +270,9 @@ TEST_F(QukeysBasic, RolloverPrimary) {
   addToReport(Key_F);
   expectReportAfterMillis(30);
   addToReport(Key_X);
-  expectReport(std::set<Key>{}, std::set<Key>{});
+  expectReport(std::set<Key> {}, std::set<Key> {});
   removeFromReport(Key_F);
-  expectReport(std::set<Key>{}, std::set<Key>{});
+  expectReport(std::set<Key> {}, std::set<Key> {});
   releaseKey(40, key_addr_X);
   removeFromReport(Key_X);
   expectReportAfterCycles(1);
