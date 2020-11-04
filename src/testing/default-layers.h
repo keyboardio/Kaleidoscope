7ef7b4ba57dfe1c298138511d5a770a6129cce0e
Michael Richters <gedankenexperimenter@gmail.com>
2020-11-03 22:41:59 -0600

    Add common keymap files to simplify test sketches
    Signed-off-by: Michael Richters <gedankenexperimenter@gmail.com>

---
+++ src/testing/default-layers.h
@@ -0,0 +1,98 @@
+// -*- c++ -*-
+#pragma once
+
+#define LAYER_0 \
+  [0] = KEYMAP_STACKED(                                                 \
+      key_0AA, key_0AB, key_0AC, key_0AD, key_0AE, key_0AF, key_0AG,    \
+      key_0BA, key_0BB, key_0BC, key_0BD, key_0BE, key_0BF, key_0BG,    \
+      key_0CA, key_0CB, key_0CC, key_0CD, key_0CE, key_0CF,             \
+      key_0DA, key_0DB, key_0DC, key_0DD, key_0DE, key_0DF, key_0CG,    \
+      key_0AH, key_0BH, key_0CH, key_0DH,                               \
+      key_0DG,                                                          \
+                                                                        \
+      key_0AJ, key_0AK, key_0AL, key_0AM, key_0AN, key_0AO, key_0AP,    \
+      key_0BJ, key_0BK, key_0BL, key_0BM, key_0BN, key_0BO, key_0BP,    \
+      key_0CK, key_0CL, key_0CM, key_0CN, key_0CO, key_0CP,             \
+      key_0CJ, key_0DK, key_0DL, key_0DM, key_0DN, key_0DO, key_0DP,    \
+      key_0DI, key_0CI, key_0BI, key_0AI,                               \
+      key_0DJ)
+
+#define LAYER_1 \
+  [1] = KEYMAP_STACKED(                                                 \
+      key_1AA, key_1AB, key_1AC, key_1AD, key_1AE, key_1AF, key_1AG,    \
+      key_1BA, key_1BB, key_1BC, key_1BD, key_1BE, key_1BF, key_1BG,    \
+      key_1CA, key_1CB, key_1CC, key_1CD, key_1CE, key_1CF,             \
+      key_1DA, key_1DB, key_1DC, key_1DD, key_1DE, key_1DF, key_1CG,    \
+      key_1AH, key_1BH, key_1CH, key_1DH,                               \
+      key_1DG,                                                          \
+                                                                        \
+      key_1AJ, key_1AK, key_1AL, key_1AM, key_1AN, key_1AO, key_1AP,    \
+      key_1BJ, key_1BK, key_1BL, key_1BM, key_1BN, key_1BO, key_1BP,    \
+      key_1CK, key_1CL, key_1CM, key_1CN, key_1CO, key_1CP,             \
+      key_1CJ, key_1DK, key_1DL, key_1DM, key_1DN, key_1DO, key_1DP,    \
+      key_1DI, key_1CI, key_1BI, key_1AI,                               \
+      key_1DJ)
+
+#define LAYER_2 \
+  [2] = KEYMAP_STACKED(                                                 \
+      key_2AA, key_2AB, key_2AC, key_2AD, key_2AE, key_2AF, key_2AG,    \
+      key_2BA, key_2BB, key_2BC, key_2BD, key_2BE, key_2BF, key_2BG,    \
+      key_2CA, key_2CB, key_2CC, key_2CD, key_2CE, key_2CF,             \
+      key_2DA, key_2DB, key_2DC, key_2DD, key_2DE, key_2DF, key_2CG,    \
+      key_2AH, key_2BH, key_2CH, key_2DH,                               \
+      key_2DG,                                                          \
+                                                                        \
+      key_2AJ, key_2AK, key_2AL, key_2AM, key_2AN, key_2AO, key_2AP,    \
+      key_2BJ, key_2BK, key_2BL, key_2BM, key_2BN, key_2BO, key_2BP,    \
+      key_2CK, key_2CL, key_2CM, key_2CN, key_2CO, key_2CP,             \
+      key_2CJ, key_2DK, key_2DL, key_2DM, key_2DN, key_2DO, key_2DP,    \
+      key_2DI, key_2CI, key_2BI, key_2AI,                               \
+      key_2DJ)
+
+#define LAYER_3 \
+  [3] = KEYMAP_STACKED(                                                 \
+      key_3AA, key_3AB, key_3AC, key_3AD, key_3AE, key_3AF, key_3AG,    \
+      key_3BA, key_3BB, key_3BC, key_3BD, key_3BE, key_3BF, key_3BG,    \
+      key_3CA, key_3CB, key_3CC, key_3CD, key_3CE, key_3CF,             \
+      key_3DA, key_3DB, key_3DC, key_3DD, key_3DE, key_3DF, key_3CG,    \
+      key_3AH, key_3BH, key_3CH, key_3DH,                               \
+      key_3DG,                                                          \
+                                                                        \
+      key_3AJ, key_3AK, key_3AL, key_3AM, key_3AN, key_3AO, key_3AP,    \
+      key_3BJ, key_3BK, key_3BL, key_3BM, key_3BN, key_3BO, key_3BP,    \
+      key_3CK, key_3CL, key_3CM, key_3CN, key_3CO, key_3CP,             \
+      key_3CJ, key_3DK, key_3DL, key_3DM, key_3DN, key_3DO, key_3DP,    \
+      key_3DI, key_3CI, key_3BI, key_3AI,                               \
+      key_3DJ)
+
+#define LAYER_4 \
+  [4] = KEYMAP_STACKED(                                                 \
+      key_4AA, key_4AB, key_4AC, key_4AD, key_4AE, key_4AF, key_4AG,    \
+      key_4BA, key_4BB, key_4BC, key_4BD, key_4BE, key_4BF, key_4BG,    \
+      key_4CA, key_4CB, key_4CC, key_4CD, key_4CE, key_4CF,             \
+      key_4DA, key_4DB, key_4DC, key_4DD, key_4DE, key_4DF, key_4CG,    \
+      key_4AH, key_4BH, key_4CH, key_4DH,                               \
+      key_4DG,                                                          \
+                                                                        \
+      key_4AJ, key_4AK, key_4AL, key_4AM, key_4AN, key_4AO, key_4AP,    \
+      key_4BJ, key_4BK, key_4BL, key_4BM, key_4BN, key_4BO, key_4BP,    \
+      key_4CK, key_4CL, key_4CM, key_4CN, key_4CO, key_4CP,             \
+      key_4CJ, key_4DK, key_4DL, key_4DM, key_4DN, key_4DO, key_4DP,    \
+      key_4DI, key_4CI, key_4BI, key_4AI,                               \
+      key_4DJ)
+
+#define LAYER_5 \
+  [5] = KEYMAP_STACKED(                                                 \
+      key_5AA, key_5AB, key_5AC, key_5AD, key_5AE, key_5AF, key_5AG,    \
+      key_5BA, key_5BB, key_5BC, key_5BD, key_5BE, key_5BF, key_5BG,    \
+      key_5CA, key_5CB, key_5CC, key_5CD, key_5CE, key_5CF,             \
+      key_5DA, key_5DB, key_5DC, key_5DD, key_5DE, key_5DF, key_5CG,    \
+      key_5AH, key_5BH, key_5CH, key_5DH,                               \
+      key_5DG,                                                          \
+                                                                        \
+      key_5AJ, key_5AK, key_5AL, key_5AM, key_5AN, key_5AO, key_5AP,    \
+      key_5BJ, key_5BK, key_5BL, key_5BM, key_5BN, key_5BO, key_5BP,    \
+      key_5CK, key_5CL, key_5CM, key_5CN, key_5CO, key_5CP,             \
+      key_5CJ, key_5DK, key_5DL, key_5DM, key_5DN, key_5DO, key_5DP,    \
+      key_5DI, key_5CI, key_5BI, key_5AI,                               \
+      key_5DJ)
