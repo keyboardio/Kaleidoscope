6ef1570964cefb2bd48e49c459a0b9bba0defe6e
Jesse Vincent <jesse@keyboard.io>
2020-11-22 00:50:40 -0800

    Improve dependency tracking, cleanup and presentation in test Makefiles.

---
+++ testing/makefiles/shared.mk
@@ -0,0 +1,21 @@
+arduino_platform_path := ${ARDUINO_DIRECTORIES_USER}/hardware/keyboardio/virtual/
+
+shared_defines := \
+                -DARDUINO=10607 \
+                -DARDUINO_ARCH_VIRTUAL \
+                -DKALEIDOSCOPE_VIRTUAL_BUILD=1 \
+                -DKEYBOARDIOHID_BUILD_WITHOUT_HID=1 \
+                -DUSBCON=dummy \
+                -DARDUINO_ARCH_AVR=1 \
+                -DARDUINO_AVR_MODEL01 \
+                '-DKALEIDOSCOPE_HARDWARE_H="Kaleidoscope-Hardware-Model01.h"' \
+                '-DUSB_PRODUCT="Model 01"'
+
+shared_includes := \
+		-I${top_dir} \
+		-I${top_dir}/src \
+		-I${arduino_platform_path}/cores/arduino \
+		-I${arduino_platform_path}/libraries/KeyboardioHID/src \
+		-I${top_dir}/testing/googletest/googlemock/include \
+		-I${top_dir}/testing/googletest/googletest/include \
+
