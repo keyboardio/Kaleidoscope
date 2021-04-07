a2f720e36515b7b7135f556d37f699b98b2c34f7
Michael Richters <gedankenexperimenter@gmail.com>
2021-04-06 19:09:30 -0500

    Add testcases for rollover conditions on Keyboard HID keys
    Signed-off-by: Michael Richters <gedankenexperimenter@gmail.com>

---
+++ tests/features/rollover/rollover.ino
@@ -0,0 +1,48 @@
+/* -*- mode: c++ -*-
+ * Copyright (C) 2020  Keyboard.io, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Kaleidoscope.h>
+
+#include "./common.h"
+
+// *INDENT-OFF*
+KEYMAPS(
+    [0] = KEYMAP_STACKED
+    (
+        LSHIFT(Key_A), LSHIFT(Key_B), LSHIFT(LCTRL(LALT(Key_X))), ___, ___, ___, ___,
+        Key_A, Key_B, ___, ___, ___, ___, ___,
+        Key_LeftShift, Key_RightShift, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___,
+        ___,
+
+        ___, ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___,
+        ___
+    ),
+)
+// *INDENT-ON*
+
+void setup() {
+  Kaleidoscope.setup();
+}
+
+void loop() {
+  Kaleidoscope.loop();
+}
