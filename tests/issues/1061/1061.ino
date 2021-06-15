25c975eae7422a7908cb0ef2d5cc01f83c1d4ac6
Michael Richters <gedankenexperimenter@gmail.com>
2021-06-14 19:49:16 -0500

    Add testcase for issue 1061
    This is a demonstration that another plugin can use the new public OneShot
    methods to turn a non-OneShot key into a OneShot.
    
    Signed-off-by: Michael Richters <gedankenexperimenter@gmail.com>

---
+++ tests/issues/1061/1061.ino
@@ -0,0 +1,70 @@
+/* -*- mode: c++ -*-
+ * Copyright (C) 2021  Keyboard.io, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Kaleidoscope.h>
+#include <Kaleidoscope-OneShot.h>
+
+// *INDENT-OFF*
+KEYMAPS(
+    [0] = KEYMAP_STACKED
+    (
+        Key_Insert, ___, ___, ___, ___, ___, ___,
+        Key_A, Key_B, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___,
+        ___,
+
+        ___, ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___, ___, ___,
+        ___, ___, ___, ___,
+        ___
+    ),
+)
+// *INDENT-ON*
+
+namespace kaleidoscope {
+namespace plugin {
+
+class OneShotInsert : public Plugin {
+ public:
+  EventHandlerResult onKeyEvent(KeyEvent &event) {
+    if (keyToggledOn(event.state) && (event.state & INJECTED) == 0 &&
+        event.key == Key_Insert && live_keys[event.addr] != event.key)
+      ::OneShot.setPending(event.addr);
+    return EventHandlerResult::OK;
+  }
+};
+
+} // namespace plugin
+} // namespace kaleidoscope
+
+kaleidoscope::plugin::OneShotInsert OneShotInsert;
+
+KALEIDOSCOPE_INIT_PLUGINS(OneShot, OneShotInsert);
+
+void setup() {
+  Kaleidoscope.setup();
+  OneShot.setTimeout(50);
+  OneShot.setHoldTimeout(20);
+  OneShot.setDoubleTapTimeout(20);
+}
+
+void loop() {
+  Kaleidoscope.loop();
+}
