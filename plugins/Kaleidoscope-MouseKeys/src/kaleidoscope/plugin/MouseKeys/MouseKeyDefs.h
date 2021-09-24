7a2de238c4286d93ece0e3ce2c530e62ce16d988
Jesse Vincent <jesse@keyboard.io>
2021-09-24 14:03:15 -0700

    Replace deprecated syntax for bitfield

--- plugins/Kaleidoscope-MouseKeys/src/kaleidoscope/plugin/MouseKeys/MouseKeyDefs.h
+++ plugins/Kaleidoscope-MouseKeys/src/kaleidoscope/plugin/MouseKeys/MouseKeyDefs.h
@@ -16,7 +16,7 @@
 
 #pragma once
 
-#define IS_MOUSE_KEY             B00010000
+#define IS_MOUSE_KEY             0b00010000
 
 // Synthetic, not internal
 #define KEY_MOUSE_BTN_L MOUSE_LEFT // Synthetic key
@@ -25,15 +25,15 @@
 #define KEY_MOUSE_BTN_P MOUSE_PREV
 #define KEY_MOUSE_BTN_N MOUSE_NEXT
 
-#define KEY_MOUSE_UP            B0000001
-#define KEY_MOUSE_DOWN          B0000010
-#define KEY_MOUSE_LEFT          B0000100
-#define KEY_MOUSE_RIGHT         B0001000
-#define KEY_MOUSE_WHEEL         B0010000
-#define KEY_MOUSE_WARP          B0100000
-#define KEY_MOUSE_WARP_END      B1000000
+#define KEY_MOUSE_UP            0b0000001
+#define KEY_MOUSE_DOWN          0b0000010
+#define KEY_MOUSE_LEFT          0b0000100
+#define KEY_MOUSE_RIGHT         0b0001000
+#define KEY_MOUSE_WHEEL         0b0010000
+#define KEY_MOUSE_WARP          0b0100000
+#define KEY_MOUSE_WARP_END      0b1000000
 // all buttons end warp. also, we're out of bits
-#define KEY_MOUSE_BUTTON        B1000000
+#define KEY_MOUSE_BUTTON        0b1000000
 
 
 #define Key_mouseWarpNW   Key(KEY_MOUSE_WARP| KEY_MOUSE_UP | KEY_MOUSE_LEFT, KEY_FLAGS|SYNTHETIC|IS_MOUSE_KEY)
