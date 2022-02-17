50ca989e77098b7e4827614ab43172b44f11a59e
Jesse Vincent <jesse@keyboard.io>
2022-02-16 18:53:26 -0800

    GD32 has customizable buffer sizes

--- plugins/KeyboardioHID/src/HID-Settings.h
+++ plugins/KeyboardioHID/src/HID-Settings.h
@@ -131,6 +131,7 @@ int USB_SendControl(uint8_t x, const void* y, uint8_t z);
 #define USB_DEVICE_CLASS_HUMAN_INTERFACE       0x03
 #endif
 
+#define ARCH_HAS_CONFIGURABLE_EP_SIZES
 
 constexpr uint16_t EP_TYPE_INTERRUPT_IN(uint8_t buffer_size) { return EPDesc(USB_TRX_IN, USB_EP_ATTR_INT, buffer_size).val; }
 constexpr uint16_t EP_TYPE_INTERRUPT_OUT(uint8_t buffer_size) { return EPDesc(USB_TRX_OUT, USB_EP_ATTR_INT, buffer_size).val; }
