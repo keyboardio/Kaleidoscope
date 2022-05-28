95b7a7b684395e4a05ade2b135685aea810d8c45
iliana etaoin <iliana@buttslol.net>
2022-05-28 15:19:25 -0700

    Add PrefixLayer plugin
    Signed-off-by: iliana etaoin <iliana@buttslol.net>

---
+++ plugins/Kaleidoscope-PrefixLayer/src/kaleidoscope/plugin/PrefixLayer.cpp
@@ -0,0 +1,77 @@
+/* -*- mode: c++ -*-
+ * Kaleidoscope-PrefixLayer -- Sends a prefix key for every key in a layer.
+ * Copyright (C) 2017, 2022  iliana etaoin <iliana@buttslol.net>
+ * Copyright (C) 2017  James Cash
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "kaleidoscope/plugin/PrefixLayer.h"
+
+#include <Arduino.h>  // for PROGMEM
+#include <stdint.h>   // for uint8_t
+
+#include "kaleidoscope/KeyEvent.h"              // for KeyEvent
+#include "kaleidoscope/Runtime.h"               // for Runtime
+#include "kaleidoscope/event_handler_result.h"  // for EventHandlerResult, EventHandlerResult::OK
+#include "kaleidoscope/key_defs.h"              // for Key
+#include "kaleidoscope/keyswitch_state.h"       // for keyToggledOn
+#include "kaleidoscope/layers.h"                // for Layer
+
+namespace kaleidoscope {
+namespace plugin {
+
+static const PrefixLayer::Entry prefix_layers_default_[] PROGMEM = {};
+const PrefixLayer::Entry *PrefixLayer::prefix_layers             = prefix_layers_default_;
+uint8_t PrefixLayer::prefix_layers_length                        = 0;
+bool PrefixLayer::clear_modifiers_                               = false;
+
+EventHandlerResult PrefixLayer::onKeyEvent(KeyEvent &event) {
+  if (event.state & INJECTED)
+    return EventHandlerResult::OK;
+  if (!keyToggledOn(event.state))
+    return EventHandlerResult::OK;
+  if (event.key == Key_NoKey)
+    return EventHandlerResult::OK;
+  if (!event.key.isKeyboardKey())
+    return EventHandlerResult::OK;
+  if (event.key.isKeyboardModifier())
+    return EventHandlerResult::OK;
+
+  for (uint8_t i = 0; i < prefix_layers_length; i++) {
+    if (Layer.isActive(prefix_layers[i].layer)) {
+      clear_modifiers_ = true;
+      Runtime.handleKeyEvent(KeyEvent{KeyAddr::none(), IS_PRESSED | INJECTED, prefix_layers[i].prefix});
+      Runtime.handleKeyEvent(KeyEvent{KeyAddr::none(), WAS_PRESSED | INJECTED, prefix_layers[i].prefix});
+      clear_modifiers_ = false;
+    }
+  }
+
+  return EventHandlerResult::OK;
+}
+
+EventHandlerResult PrefixLayer::beforeReportingState(const KeyEvent &event) {
+  if (clear_modifiers_) {
+    for (uint8_t i = HID_KEYBOARD_FIRST_MODIFIER; i <= HID_KEYBOARD_LAST_MODIFIER; i++) {
+      Runtime.hid().keyboard().releaseKey(Key(i, KEY_FLAGS));
+    }
+    Runtime.hid().keyboard().pressModifiers(event.key);
+  }
+
+  return EventHandlerResult::OK;
+}
+
+}  // namespace plugin
+}  // namespace kaleidoscope
+
+kaleidoscope::plugin::PrefixLayer PrefixLayer;
---
+++ plugins/Kaleidoscope-PrefixLayer/src/kaleidoscope/plugin/PrefixLayer.h
@@ -0,0 +1,54 @@
+/* -*- mode: c++ -*-
+ * Kaleidoscope-PrefixLayer -- Sends a prefix key for every key in a layer.
+ * Copyright (C) 2017, 2022  iliana etaoin <iliana@buttslol.net>
+ * Copyright (C) 2017  James Cash
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <stdint.h>  // for uint8_t
+
+#include "kaleidoscope/KeyEvent.h"              // for KeyEvent
+#include "kaleidoscope/event_handler_result.h"  // for EventHandlerResult
+#include "kaleidoscope/key_defs.h"              // for Key
+#include "kaleidoscope/plugin.h"                // for Plugin
+
+namespace kaleidoscope {
+namespace plugin {
+
+class PrefixLayer : public Plugin {
+ public:
+  EventHandlerResult onKeyEvent(KeyEvent &event);
+  EventHandlerResult beforeReportingState(const KeyEvent &event);
+
+  struct Entry {
+    uint8_t layer;
+    Key prefix;
+
+    constexpr Entry(uint8_t layer, Key prefix)
+      : layer(layer), prefix(prefix) {}
+  };
+
+  static const Entry *prefix_layers;
+  static uint8_t prefix_layers_length;
+
+ private:
+  static bool clear_modifiers_;
+};
+
+}  // namespace plugin
+}  // namespace kaleidoscope
+
+extern kaleidoscope::plugin::PrefixLayer PrefixLayer;
