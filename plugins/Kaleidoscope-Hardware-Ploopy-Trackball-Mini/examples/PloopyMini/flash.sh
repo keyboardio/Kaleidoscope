f87ff8ae1847a4ffbf10e4ddf457152d0bfcbdfd
Gergely Nagy <algernon@keyboard.io>
2022-06-29 00:57:53 +0200

    wip: ploopymini flash.sh
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ plugins/Kaleidoscope-Hardware-Ploopy-Trackball-Mini/examples/PloopyMini/flash.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+set -e
+
+echo "* Building"
+make OUTPUT_PATH=$(pwd)/_build >/dev/null
+
+echo "* Jumping to bootloader..."
+DEVICE=/dev/ttyACM1 focus device.reset
+sleep 2s
+
+echo "* Flashing..."
+dfu-programmer atmega32u4 erase && dfu-programmer atmega32u4 flash _build/PloopyMini-latest.hex && dfu-programmer atmega32u4 start
