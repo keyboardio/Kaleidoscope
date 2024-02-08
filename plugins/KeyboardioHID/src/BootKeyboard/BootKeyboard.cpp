dec5c2937af7f6159b113afdd42942d740b4f519
Taylor Yu <code@argon.blue>
2024-02-07 18:05:35 -0600

    BootKeyboardAPI
    Signed-off-by: Taylor Yu <code@argon.blue>

--- plugins/KeyboardioHID/src/BootKeyboard/BootKeyboard.cpp
+++ plugins/KeyboardioHID/src/BootKeyboard/BootKeyboard.cpp
@@ -37,30 +37,15 @@ static const uint8_t hybrid_keyboard_hid_descriptor_[] PROGMEM = {
 };
 
 BootKeyboard_::BootKeyboard_(uint8_t bootkb_only_)
-  : bootkb_only(bootkb_only_) {
+  : BootKeyboardAPI(bootkb_only_) {
 
   itfProtocol = HID_PROTOCOL_KEYBOARD;
   setBootOnly(bootkb_only);
   plug();
 }
 
-
-void BootKeyboard_::begin() {
-}
-
-
-void BootKeyboard_::end() {
-  releaseAll();
-  sendReport();
-}
-
-
 uint8_t BootKeyboard_::getLeds() {
   return outReport[0];
-}
-
-uint8_t BootKeyboard_::getBootOnly() {
-  return bootkb_only;
 }
 
 /*
@@ -73,8 +58,8 @@ uint8_t BootKeyboard_::getBootOnly() {
  * expects, and the host probably won't see the new Report Descriptor
  * until after a re-attach.
  */
-void BootKeyboard_::setBootOnly(uint8_t bootonly) {
-  bootkb_only = bootonly;
+
+void BootKeyboard_::setReportDescriptor(uint8_t bootkb_only) {
   if (bootkb_only) {
     reportDesc     = boot_keyboard_hid_descriptor_;
     descriptorSize = sizeof(boot_keyboard_hid_descriptor_);
@@ -84,223 +69,9 @@ void BootKeyboard_::setBootOnly(uint8_t bootonly) {
   }
 }
 
-
-void BootKeyboard_::convertReport(uint8_t *boot, const uint8_t *nkro) {
-  uint8_t n_boot_keys = 0;
-  uint8_t b;
-  // Convert NKRO report to boot report
-  memset(boot, HID_KEYBOARD_NO_EVENT, BOOT_KEY_BYTES);
-  for (uint8_t i = 0; i < NKRO_KEY_BYTES; i++) {
-    b = nkro[i];
-    if (b == 0) {
-      continue;
-    }
-    for (uint8_t j = 0; j < 8; j++) {
-      bool bit = b & 1;
-      b >>= 1;
-      if (bit == 0) {
-        continue;
-      }
-      // Check is here so we can set all BOOT_KEY_BYTES
-      if (n_boot_keys >= BOOT_KEY_BYTES) {
-        // Send rollover error if too many keys are held
-        memset(boot, HID_KEYBOARD_ERROR_ROLLOVER, BOOT_KEY_BYTES);
-        return;
-      }
-      boot[n_boot_keys++] = 8 * i + j;
-    }
-  }
-}
-
-/* Send a report without the extra modifier change handling */
-int BootKeyboard_::sendReportUnchecked() {
-  HID_BootKeyboardReport_Data_t out_report;
-  out_report.modifiers = last_report_.modifiers;
-  out_report.reserved  = 0;
-  memcpy(out_report.nkro_keys, last_report_.keys, sizeof(last_report_.keys));
-  convertReport(out_report.boot_keycodes, out_report.nkro_keys);
-  size_t reportlen;
-  // Send only boot report if host requested boot protocol, or if configured as boot-only
-  if (protocol == HID_BOOT_PROTOCOL || bootkb_only) {
-    reportlen = BOOT_REPORT_LEN;
-  } else {
-    reportlen = sizeof(out_report);
-  }
-  int returnCode = SendReport(0, &out_report, reportlen);
-  HIDReportObserver::observeReport(HID_REPORTID_KEYBOARD, &out_report, reportlen, returnCode);
-  return returnCode;
-}
-
-// Sending the current HID report to the host:
-//
-// Depending on the differences between the current and previous HID reports, we
-// might need to send one or two extra reports to guarantee that the host will
-// process the changes in the correct order. There are two important scenarios
-// to consider:
-//
-// 1. If a non-modifier keycode toggles off in the same report as a modifier
-// changes, the host might process the modifier change first. For example, if
-// both `shift` and `4` toggle off in the same report (most likely from a
-// `LSHIFT(Key_4)` key being released), and that key has been held long enough
-// to trigger character repeat, we could end up with a plain `4` in the output
-// at the end of the repeat: `$$$$4` instead of `$$$$$`.
-//
-// 2. If a non-modifier keycode toggles on in the same report as a modifier
-// changes, the host might process the non-modifer first. For example, pressing
-// and holding an `LSHIFT(Key_4)` key might result in `4$$$` rather than `$$$$`.
-//
-// Therefore, each call to `sendReport()` must send (up to) three reports to the
-// host to guarantee the correct order of processing:
-//
-// 1. A report with toggled-off non-modifiers removed.
-// 2. A report with changes to modifiers.
-// 3. A report with toggled-on non-modifiers added.
-
-int BootKeyboard_::sendReport() {
-  // If the new HID report differs from the previous one both in active modifier
-  // keycodes and non-modifier keycodes, we will need to send at least one extra
-  // report. First, we compare the modifiers bytes of the two reports.
-  const uint8_t old_modifiers = last_report_.modifiers;
-  const uint8_t new_modifiers = report_.modifiers;
-
-  const uint8_t changed_modifiers = old_modifiers ^ new_modifiers;
-
-  if (changed_modifiers != 0) {
-    // There was at least one modifier change (toggled on or off), remove any
-    // non-modifiers from the stored previous report that toggled off in the new
-    // report, and send it to the host.
-    bool non_modifiers_toggled_off = false;
-    for (uint8_t i = 0; i < NKRO_KEY_BYTES; ++i) {
-      byte released_keycodes = last_report_.keys[i] & ~(report_.keys[i]);
-      if (released_keycodes != 0) {
-        last_report_.keys[i] &= ~released_keycodes;
-        non_modifiers_toggled_off = true;
-      }
-    }
-    if (non_modifiers_toggled_off) {
-      sendReportUnchecked();
-    }
-    // Next, update the modifiers byte of the stored previous report, and send
-    // it.
-    last_report_.modifiers = new_modifiers;
-    sendReportUnchecked();
-  }
-
-  // Finally, copy the new report to the previous one, and send it.
-  if (memcmp(last_report_.keys, report_.keys, sizeof(report_.keys)) != 0) {
-    memcpy(last_report_.keys, report_.keys, sizeof(report_.keys));
-    return sendReportUnchecked();
-  }
-  // A note on return values: Kaleidoscope doesn't actually check the return
-  // value of `sendReport()`, so this function could be changed to return
-  // void. It would be nice if we could do something to recover from an error
-  // here, but it's not at all clear what that should be. Also note that if the
-  // extra reports above return an error, there's not much we can do to try to
-  // recover. We could try to send the report again, but that would be likely to
-  // fail as well.
-  return -1;
-}
-
-/* Returns true if the modifer key passed in will be sent during this key report
- * Returns false in all other cases
- * */
-bool BootKeyboard_::isModifierActive(uint8_t k) {
-  if (k >= HID_KEYBOARD_FIRST_MODIFIER && k <= HID_KEYBOARD_LAST_MODIFIER) {
-    k = k - HID_KEYBOARD_FIRST_MODIFIER;
-    return !!(report_.modifiers & (1 << k));
-  }
-  return false;
-}
-
-/* Returns true if the modifer key passed in was being sent during the previous key report
- * Returns false in all other cases
- * */
-bool BootKeyboard_::wasModifierActive(uint8_t k) {
-  if (k >= HID_KEYBOARD_FIRST_MODIFIER && k <= HID_KEYBOARD_LAST_MODIFIER) {
-    k = k - HID_KEYBOARD_FIRST_MODIFIER;
-    return !!(last_report_.modifiers & (1 << k));
-  }
-  return false;
-}
-
-/* Returns true if *any* modifier will be sent during this key report
- * Returns false in all other cases
- * */
-bool BootKeyboard_::isAnyModifierActive() {
-  return report_.modifiers > 0;
-}
-
-/* Returns true if *any* modifier was being sent during the previous key report
- * Returns false in all other cases
- * */
-bool BootKeyboard_::wasAnyModifierActive() {
-  return last_report_.modifiers > 0;
-}
-
-
-/* Returns true if the non-modifier key passed in will be sent during this key report
- * Returns false in all other cases
- * */
-bool BootKeyboard_::isKeyPressed(uint8_t k) {
-  if (k <= HID_LAST_KEY) {
-    uint8_t bit = 1 << (uint8_t(k) % 8);
-    return !!(report_.keys[k / 8] & bit);
-  }
-  return false;
-}
-
-/* Returns true if the non-modifer key passed in was sent during the previous key report
- * Returns false in all other cases
- * */
-bool BootKeyboard_::wasKeyPressed(uint8_t k) {
-
-  if (k <= HID_LAST_KEY) {
-    uint8_t bit = 1 << (uint8_t(k) % 8);
-    return !!(last_report_.keys[k / 8] & bit);
-  }
-  return false;
-}
-
-
-size_t BootKeyboard_::press(uint8_t k) {
-  // If the key is in the range of 'printable' keys
-  if (k <= HID_LAST_KEY) {
-    uint8_t bit = 1 << (uint8_t(k) % 8);
-    report_.keys[k / 8] |= bit;
-    return 1;
-  } else if (k >= HID_KEYBOARD_FIRST_MODIFIER && k <= HID_KEYBOARD_LAST_MODIFIER) {
-    // It's a modifier key
-    // Convert key into bitfield (0 - 7)
-    k = k - HID_KEYBOARD_FIRST_MODIFIER;
-    report_.modifiers |= (1 << k);
-    return 1;
-  }
-
-  // No empty/pressed key was found
-  return 0;
-}
-
-size_t BootKeyboard_::release(uint8_t k) {
-  // If we're releasing a printable key
-  if (k <= HID_LAST_KEY) {
-    uint8_t bit = 1 << (k % 8);
-    report_.keys[k / 8] &= ~bit;
-    return 1;
-  } else if (k >= HID_KEYBOARD_FIRST_MODIFIER && k <= HID_KEYBOARD_LAST_MODIFIER) {
-    // It's a modifier key
-    // Convert key into bitfield (0 - 7)
-    k = k - HID_KEYBOARD_FIRST_MODIFIER;
-    report_.modifiers &= ~(1 << k);
-    return 1;
-  }
-
-  // No empty/pressed key was found
-  return 0;
-}
-
-void BootKeyboard_::releaseAll() {
-  // Release all keys
-  memset(&report_.allkeys, 0x00, sizeof(report_.allkeys));
+int BootKeyboard_::SendReport(const void *data, int len) {
+  int returnCode = HIDD::SendReport(0, data, len);
+  HIDReportObserver::observeReport(HID_REPORTID_KEYBOARD, data, len, returnCode);
 }
 
 /*
--- plugins/KeyboardioHID/src/BootKeyboard/BootKeyboard.h
+++ plugins/KeyboardioHID/src/BootKeyboard/BootKeyboard.h
@@ -27,188 +27,30 @@ THE SOFTWARE.
 #pragma once
 
 #include <Arduino.h>
+#include "kaleidoscope/driver/hid/apis/BootKeyboardAPI.h"
 #include "HID.h"
 #include "HIDD.h"
 #include "HIDTables.h"
 #include "HIDAliases.h"
 
-// Declare the hybrid boot keyboard feature so Kaleidoscope can depend on it
-#define BOOTKB_HYBRID   1
 
-#define BOOT_KEY_BYTES  6
-#define BOOT_REPORT_LEN 8
-
-#define NKRO_KEY_BITS   (4 + HID_LAST_KEY - HID_KEYBOARD_A_AND_A + 1)
-#define NKRO_KEY_BYTES  ((NKRO_KEY_BITS + 7) / 8)
-
-// See Appendix B of USB HID spec
-#define DESCRIPTOR_BOOT_KEYBOARD(...)                   \
-  HID_USAGE_PAGE(HID_USAGE_PAGE_DESKTOP),               \
-    HID_USAGE(HID_USAGE_DESKTOP_KEYBOARD),              \
-    HID_COLLECTION(HID_COLLECTION_APPLICATION),         \
-                                                        \
-    /* Report ID, if any */                             \
-    __VA_ARGS__                                         \
-                                                        \
-      /* LEDs */                                        \
-      HID_REPORT_COUNT(8),                              \
-    HID_REPORT_SIZE(1),                                 \
-    HID_USAGE_PAGE(HID_USAGE_PAGE_LED),                 \
-    HID_USAGE_MIN(1),                                   \
-    HID_USAGE_MAX(8),                                   \
-    HID_LOGICAL_MIN(0),                                 \
-    HID_LOGICAL_MAX(1),                                 \
-    HID_OUTPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE), \
-                                                        \
-    /* Modifiers */                                     \
-    HID_USAGE_PAGE(HID_USAGE_PAGE_KEYBOARD),            \
-    HID_USAGE_MIN(HID_KEYBOARD_FIRST_MODIFIER),         \
-    HID_USAGE_MAX(HID_KEYBOARD_LAST_MODIFIER),          \
-    HID_INPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE),  \
-                                                        \
-    /* Reserved byte */                                 \
-    HID_REPORT_COUNT(1),                                \
-    HID_REPORT_SIZE(8),                                 \
-    HID_INPUT(HID_CONSTANT),                            \
-                                                        \
-    /* Non-modifiers */                                 \
-    HID_REPORT_COUNT(BOOT_KEY_BYTES),                   \
-    HID_LOGICAL_MAX_N(HID_LAST_KEY, 2),                 \
-    HID_USAGE_MIN(HID_FIRST_KEY),                       \
-    HID_USAGE_MAX(HID_LAST_KEY),                        \
-    HID_INPUT(HID_DATA | HID_ARRAY | HID_ABSOLUTE),     \
-                                                        \
-    HID_COLLECTION_END
-
-// Padding to round up the report to byte boundary.
-#if (NKRO_KEY_BITS % 8)
-#define DESCRIPTOR_HYBRID_KEYBOARD_PADDING   \
-  HID_REPORT_COUNT(8 - (NKRO_KEY_BITS % 8)), \
-    HID_INPUT(HID_CONSTANT),
-#else
-#define DESCRIPTOR_HYBRID_KEYBOARD_PADDING /* empty */
-#endif
-
-#define DESCRIPTOR_HYBRID_KEYBOARD(...)                                       \
-  HID_USAGE_PAGE(HID_USAGE_PAGE_DESKTOP),                                     \
-    HID_USAGE(HID_USAGE_DESKTOP_KEYBOARD),                                    \
-    HID_COLLECTION(HID_COLLECTION_APPLICATION),                               \
-                                                                              \
-    /* Report ID, if any */                                                   \
-    __VA_ARGS__                                                               \
-                                                                              \
-    /* 5 LEDs for num lock etc, 3 left for advanced, custom usage */          \
-    HID_USAGE_PAGE(HID_USAGE_PAGE_LED),                                       \
-    HID_USAGE_MIN(1),                                                         \
-    HID_USAGE_MAX(8),                                                         \
-    HID_LOGICAL_MIN(0),                                                       \
-    HID_LOGICAL_MAX(1),                                                       \
-    HID_REPORT_SIZE(1),                                                       \
-    HID_REPORT_COUNT(8),                                                      \
-    HID_OUTPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE),                       \
-                                                                              \
-    /* Key modifier byte for both boot and NKRO */                            \
-    HID_USAGE_PAGE(HID_USAGE_PAGE_KEYBOARD),                                  \
-    HID_USAGE_MIN(HID_KEYBOARD_FIRST_MODIFIER),                               \
-    HID_USAGE_MAX(HID_KEYBOARD_LAST_MODIFIER),                                \
-    HID_INPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE),                        \
-                                                                              \
-    /* Send rest of boot report as padding, so HID-aware hosts will ignore */ \
-    HID_REPORT_SIZE(8),                                                       \
-    HID_REPORT_COUNT(7),                                                      \
-    HID_INPUT(HID_CONSTANT),                                                  \
-                                                                              \
-    /* NKRO key bitmap */                                                     \
-                                                                              \
-    /* Padding 4 bits, to skip NO_EVENT & 3 error states. */                  \
-    HID_REPORT_SIZE(1),                                                       \
-    HID_REPORT_COUNT(4),                                                      \
-    HID_INPUT(HID_CONSTANT),                                                  \
-                                                                              \
-    /* Actual non-modifier keys */                                            \
-    HID_USAGE_MIN(HID_KEYBOARD_A_AND_A),                                      \
-    HID_USAGE_MAX(HID_LAST_KEY),                                              \
-    HID_REPORT_COUNT(NKRO_KEY_BITS - 4),                                      \
-    HID_INPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE),                        \
-                                                                              \
-    /* Padding (if needed )*/                                                 \
-    DESCRIPTOR_HYBRID_KEYBOARD_PADDING                                        \
-                                                                              \
-      HID_COLLECTION_END
-
-/*
- * Keep the current key states in a NKRO bitmap. We'll convert it to Boot
- * Protocol as needed.
- */
-typedef union {
-  // Modifiers + keymap
-  struct {
-    uint8_t modifiers;
-    uint8_t keys[NKRO_KEY_BYTES];
-  };
-  uint8_t allkeys[1 + NKRO_KEY_BYTES];
-} HID_NKRO_KeyboardReport_Data_t;
-
-/*
- * Hybrid boot report that contains the Boot Protocol report as a prefix to
- * a bitmap NKRO report. The keycodes array of the Boot Report is marked as
- * padding in the Report Descriptor, so HID-aware hosts will ignore it, but
- * hosts that require Boot Protocol without requesting it have a chance of
- * working, assuming they can deal with the extended report.
- *
- * We do send only the Boot Report if the host has requested Boot Protocol.
- */
-typedef union {
-  // Hybrid report: boot keyboard report + NKRO report
-  struct {
-    // Boot/NKRO keyboard modifiers
-    uint8_t modifiers;
-    uint8_t reserved;
-    // Boot keyboard non-modifier keys array
-    uint8_t boot_keycodes[BOOT_KEY_BYTES];
-    // NKRO keyboard non-modifiers keys bitmap
-    uint8_t nkro_keys[NKRO_KEY_BYTES];
-  };
-  uint8_t bytes[BOOT_REPORT_LEN + NKRO_KEY_BYTES];
-} HID_BootKeyboardReport_Data_t;
-
-
-class BootKeyboard_ : public HIDD {
+class BootKeyboard_ : public HIDD, public BootKeyboardAPI {
  public:
   /*
    * Change to `bootkb_only_ = 1` if you need to default to only sending
    * Boot Protocol, even if in Report Protocol.
    */
   explicit BootKeyboard_(uint8_t bootkb_only_ = 0);
-  size_t press(uint8_t k);
-  void begin();
-  void end();
-  size_t release(uint8_t k);
-  void releaseAll();
-
-  int sendReport();
-
-  bool isModifierActive(uint8_t k);
-  bool wasModifierActive(uint8_t k);
-  bool isAnyModifierActive();
-  bool wasAnyModifierActive();
-  bool isKeyPressed(uint8_t k);
-  bool wasKeyPressed(uint8_t k);
 
   uint8_t getLeds();
 
-  uint8_t getBootOnly();
-  void setBootOnly(uint8_t bootonly);
-
   void onUSBReset();
+  uint8_t getProtocol() override {
+    return protocol;
+  }
 
  protected:
-  HID_NKRO_KeyboardReport_Data_t report_, last_report_;
-
-  uint8_t bootkb_only;
-
- private:
-  void convertReport(uint8_t *boot, const uint8_t *nkro);
-  int sendReportUnchecked();
+  int SendReport(const void *data, int len) override;
+  void setReportDescriptor(uint8_t bootkb_only) override;
 };
 extern BootKeyboard_ &BootKeyboard();
