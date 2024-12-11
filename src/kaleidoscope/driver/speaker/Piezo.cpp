03b0bf6b7be27acfc4f507618f4449000ea5dcda
Jesse Vincent <jesse@keyboard.io>
2024-12-10 19:13:26 -0800

    Initial implementation of a speaker driver

---
+++ src/kaleidoscope/driver/speaker/Piezo.cpp
@@ -0,0 +1,33 @@
+/* Kaleidoscope - Firmware for computer input devices
+ * Copyright (C) 2013-2025 Keyboard.io, inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * Additional Permissions:
+ * As an additional permission under Section 7 of the GNU General Public
+ * License Version 3, you may link this software against a Vendor-provided
+ * Hardware Specific Software Module under the terms of the MCU Vendor
+ * Firmware Library Additional Permission Version 1.0.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "kaleidoscope/driver/speaker/Piezo.h"
+
+namespace kaleidoscope {
+namespace driver {
+namespace speaker {
+
+// No explicit instantiations in core driver code
+
+}  // namespace speaker
+}  // namespace driver
+}  // namespace kaleidoscope
