993b7af5ed903568649a92e209fc94ce6f835b86
Jesse Vincent <jesse@keyboard.io>
2025-02-11 12:16:13 -0800

    Kaleidoscope-PrefixLayer: Update license to include additional permission

--- plugins/Kaleidoscope-PrefixLayer/src/kaleidoscope/plugin/PrefixLayer.h
+++ plugins/Kaleidoscope-PrefixLayer/src/kaleidoscope/plugin/PrefixLayer.h
@@ -1,11 +1,16 @@
-/* -*- mode: c++ -*-
- * Kaleidoscope-PrefixLayer -- Sends a prefix key for every key in a layer.
+/* Kaleidoscope-PrefixLayer -- Sends a prefix key for every key in a layer.
  * Copyright (C) 2017, 2022  iliana etaoin <iliana@buttslol.net>
  * Copyright (C) 2017  James Cash
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, version 3.
+ *
+ * Additional Permissions:
+ * As an additional permission under Section 7 of the GNU General Public
+ * License Version 3, you may link this software against a Vendor-provided
+ * Hardware Specific Software Module under the terms of the MCU Vendor
+ * Firmware Library Additional Permission Version 1.0.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -15,7 +20,6 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
 
 #include <stdint.h>  // for uint8_t
