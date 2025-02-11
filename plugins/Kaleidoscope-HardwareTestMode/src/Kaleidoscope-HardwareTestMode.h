cbc04a24bbba46acb3dcf36df6b525f95f0b667d
Jesse Vincent <jesse@keyboard.io>
2025-02-11 10:31:39 -0800

    Kaleidoscope-HardwareTestMode: Update license to include additional permission

--- plugins/Kaleidoscope-HardwareTestMode/src/Kaleidoscope-HardwareTestMode.h
+++ plugins/Kaleidoscope-HardwareTestMode/src/Kaleidoscope-HardwareTestMode.h
@@ -1,9 +1,15 @@
 /* Kaleidoscope-HardwareTestMode -- A factory test mode for keyboards with RGB LEDs
- * Copyright (C) 2017-2019  Keyboard.io, Inc.
+ * Copyright 2017-2025 Keyboard.io, inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, version 3.
+ *
+ * Additional Permissions:
+ * As an additional permission under Section 7 of the GNU General Public
+ * License Version 3, you may link this software against a Vendor-provided
+ * Hardware Specific Software Module under the terms of the MCU Vendor
+ * Firmware Library Additional Permission Version 1.0.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
