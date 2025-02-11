6438429dd9368a08a0003797e70116ee6d297ff6
Jesse Vincent <jesse@keyboard.io>
2025-02-11 11:14:34 -0800

    Kaleidoscope-Turbo: Update license to include additional permission

--- plugins/Kaleidoscope-Turbo/src/kaleidoscope/plugin/Turbo.h
+++ plugins/Kaleidoscope-Turbo/src/kaleidoscope/plugin/Turbo.h
@@ -1,10 +1,15 @@
-/* -*- mode: c++ -*-
- * Kaleidoscope-Turbo
+/* Kaleidoscope-Turbo -- Faster key repeats
  * Copyright (C) 2018 ash lea
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, version 3.
+ *
+ * Additional Permissions:
+ * As an additional permission under Section 7 of the GNU General Public
+ * License Version 3, you may link this software against a Vendor-provided
+ * Hardware Specific Software Module under the terms of the MCU Vendor
+ * Firmware Library Additional Permission Version 1.0.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -14,7 +19,6 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
 
 #include <Kaleidoscope-Ranges.h>  // for TURBO
