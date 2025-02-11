d9b79d00b7bd08fc3802c438fc1786102301226f
Jesse Vincent <jesse@keyboard.io>
2025-02-11 10:25:16 -0800

    Kaleidoscope-FirmwareVersion: Update license to include additional permission

--- plugins/Kaleidoscope-FirmwareVersion/src/kaleidoscope/plugin/FirmwareVersion.h
+++ plugins/Kaleidoscope-FirmwareVersion/src/kaleidoscope/plugin/FirmwareVersion.h
@@ -1,10 +1,15 @@
-/* -*- mode: c++ -*-
- * Kaleidoscope-FirmwareVersion -- Provides a Focus command to print a version
- * Copyright (C) 2022  Keyboard.io, Inc
+/* Kaleidoscope-FirmwareVersion -- Provides a Focus command to print a preconfigured version
+ * Copyright 2022-2025 Keyboard.io, inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, version 3.
+ *
+ * Additional Permissions:
+ * As an additional permission under Section 7 of the GNU General Public
+ * License Version 3, you may link this software against a Vendor-provided
+ * Hardware Specific Software Module under the terms of the MCU Vendor
+ * Firmware Library Additional Permission Version 1.0.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
