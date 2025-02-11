b4b061795a1661773d790f27aea935a70451f9e6
Jesse Vincent <jesse@keyboard.io>
2025-02-11 12:40:10 -0800

    Kaleidoscope-Hardware-Keyboardio-Alto: Update license to include additional permission

--- plugins/Kaleidoscope-Hardware-Keyboardio-Alto/examples/Devices/Keyboardio/Alto/Alto.ino
+++ plugins/Kaleidoscope-Hardware-Keyboardio-Alto/examples/Devices/Keyboardio/Alto/Alto.ino
@@ -1,12 +1,23 @@
-// -*- mode: c++ -*-
-// Copyright 2016-2022 Keyboardio, inc. <jesse@keyboard.io>
-// See "LICENSE" for license details
-
-/**
- * These #include directives pull in the Kaleidoscope firmware core,
- * as well as the Kaleidoscope plugins we use in the Alto's Firmware
+/* Kaleidoscope-Hardware-Keyboardio-Alto -- Keyboardio Alto hardware support for Kaleidoscope
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * Additional Permissions:
+ * As an additional permission under Section 7 of the GNU General Public
+ * License Version 3, you may link this software against a Vendor-provided
+ * Hardware Specific Software Module under the terms of the MCU Vendor
+ * Firmware Library Additional Permission Version 1.0.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 // The Kaleidoscope core
 #include "Kaleidoscope.h"
 
