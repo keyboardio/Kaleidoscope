ddd37c7a41ef8a5f063a74b41fd87d338e8112af
Jesse Vincent <jesse@keyboard.io>
2025-02-11 10:27:32 -0800

    Kaleidoscope-Hardware-Keyboardio-Model100: Update license to include additional permission

--- plugins/Kaleidoscope-Hardware-Keyboardio-Model100/src/kaleidoscope/device/keyboardio/Model100.cpp
+++ plugins/Kaleidoscope-Hardware-Keyboardio-Model100/src/kaleidoscope/device/keyboardio/Model100.cpp
@@ -1,10 +1,15 @@
-/* -*- mode: c++ -*-
- * Kaleidoscope-Hardware-Model100 -- Keyboardio Model 100 hardware support for Kaleidoscope
- * Copyright (C) 2021  Keyboard.io, Inc
+/* Kaleidoscope-Hardware-Keyboardio-Model100 -- Keyboardio Model100 hardware support for Kaleidoscope
+ * Copyright 2021-2025 Keyboard.io, inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, version 3.
+ *
+ * Additional Permissions:
+ * As an additional permission under Section 7 of the GNU General Public
+ * License Version 3, you may link this software against a Vendor-provided
+ * Hardware Specific Software Module under the terms of the MCU Vendor
+ * Firmware Library Additional Permission Version 1.0.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
