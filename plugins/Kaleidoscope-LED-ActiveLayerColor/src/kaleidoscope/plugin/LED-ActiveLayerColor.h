5ac5579547b116249c2ad67f309f7cebd343e0d3
Jesse Vincent <jesse@keyboard.io>
2025-02-11 10:32:41 -0800

    Kaleidoscope-LED-ActiveLayerColor: Update license to include additional permission

--- plugins/Kaleidoscope-LED-ActiveLayerColor/src/kaleidoscope/plugin/LED-ActiveLayerColor.h
+++ plugins/Kaleidoscope-LED-ActiveLayerColor/src/kaleidoscope/plugin/LED-ActiveLayerColor.h
@@ -1,17 +1,22 @@
-/* -*- mode: c++ -*-
- * Kaleidoscope-LED-ActiveLayerColor -- Light up the LEDs based on the active layers
- * Copyright (C) 2016, 2017, 2018  Keyboard.io, Inc
+/* Kaleidoscope-LED-ActiveLayerColor -- Light up the LEDs based on the active layers
+ * Copyright 2016-2025 Keyboard.io, inc.
  *
- * This program is free software: you can redistribute it and/or modify it under it under
+ * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, version 3.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT but WITHOUT
+ * Additional Permissions:
+ * As an additional permission under Section 7 of the GNU General Public
+ * License Version 3, you may link this software against a Vendor-provided
+ * Hardware Specific Software Module under the terms of the MCU Vendor
+ * Firmware Library Additional Permission Version 1.0.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public License along with along with
+ * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
