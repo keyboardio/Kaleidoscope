1a552f242bb0e518a1a39af45779b4269623fee5
Jesse Vincent <jesse@keyboard.io>
2025-02-11 11:01:00 -0800

    Kaleidoscope-LEDEffect-DigitalRain: Update license to include additional permission

--- plugins/Kaleidoscope-LEDEffect-DigitalRain/src/kaleidoscope/plugin/LEDEffect-DigitalRain.h
+++ plugins/Kaleidoscope-LEDEffect-DigitalRain/src/kaleidoscope/plugin/LEDEffect-DigitalRain.h
@@ -1,9 +1,15 @@
-/* Kaleidoscope - Firmware for computer input devices
+/* Kaleidoscope-LEDEffect-DigitalRain -- A digital rain LED effect for Kaleidoscope.
  * Copyright (C) 2017-2020  Bart Nagel
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, version 3.
+ *
+ * Additional Permissions:
+ * As an additional permission under Section 7 of the GNU General Public
+ * License Version 3, you may link this software against a Vendor-provided
+ * Hardware Specific Software Module under the terms of the MCU Vendor
+ * Firmware Library Additional Permission Version 1.0.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -13,7 +19,6 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
 
 #include "kaleidoscope/Runtime.h"
