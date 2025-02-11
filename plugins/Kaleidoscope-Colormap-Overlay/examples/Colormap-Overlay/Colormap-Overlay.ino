f39998a522e7b1f6be8b89a7e483af6b2583f92e
Jesse Vincent <jesse@keyboard.io>
2025-02-11 12:18:38 -0800

    Kaleidoscope-Colormap-Overlay: Update license to include additional permission

--- plugins/Kaleidoscope-Colormap-Overlay/examples/Colormap-Overlay/Colormap-Overlay.ino
+++ plugins/Kaleidoscope-Colormap-Overlay/examples/Colormap-Overlay/Colormap-Overlay.ino
@@ -1,10 +1,15 @@
-// -*- mode: c++ -*-
-/* Kaleidoscope - Firmware for computer input devices
+/* Kaleidoscope-Colormap-Overlay -- Per key colors overlaying active LED effect
  * Copyright (C) 2017-2020  Bart Nagel
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, version 3.
+ *
+ * Additional Permissions:
+ * As an additional permission under Section 7 of the GNU General Public
+ * License Version 3, you may link this software against a Vendor-provided
+ * Hardware Specific Software Module under the terms of the MCU Vendor
+ * Firmware Library Additional Permission Version 1.0.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -14,7 +19,6 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include <Kaleidoscope.h>
 #include <Kaleidoscope-LEDControl.h>
 #include <Kaleidoscope-LED-Palette-Theme.h>
