51efde19939bb03ca44352b609484544fb4cbfef
Jesse Vincent <jesse@keyboard.io>
2025-02-11 11:20:08 -0800

    Kaleidoscope-Chord: Update license to include additional permission

--- plugins/Kaleidoscope-Chord/src/kaleidoscope/plugin/Chord.cpp
+++ plugins/Kaleidoscope-Chord/src/kaleidoscope/plugin/Chord.cpp
@@ -1,21 +1,24 @@
-/* -*- mode: c++ -*-
- * Kaleidoscope-Chord -- Respond to chords of keys as a single keystroke
+/* Kaleidoscope-Chord -- Respond to chords of keys as a single keystroke
  * Copyright (C) 2023 Lisa Ugray
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3 or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Additional Permissions:
+ * As an additional permission under Section 7 of the GNU General Public
+ * License Version 3, you may link this software against a Vendor-provided
+ * Hardware Specific Software Module under the terms of the MCU Vendor
+ * Firmware Library Additional Permission Version 1.0.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include "kaleidoscope/plugin/Chord.h"
 
 #include <Arduino.h>  // for PROGMEM
