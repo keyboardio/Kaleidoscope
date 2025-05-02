a1a78b59b1ac3b5d386c856808111e88e3b99687
Jesse Vincent <jesse@keyboard.io>
2025-05-02 09:38:27 -0700

    Kaleidoscope core. format. no code changes

--- src/kaleidoscope/hooks.cpp
+++ src/kaleidoscope/hooks.cpp
@@ -26,7 +26,7 @@
 #include "kaleidoscope/macro_helpers.h"         // for __NL__, MAKE_TEMPLATE_SIGNATURE, UNWRAP
 
 namespace kaleidoscope {
-  
+
 
 // The following weak symbols are overwritten by using the
 // KALEIDOSCOPE_INIT_PLUGINS(...) macro in the firmware sketch. Their only
