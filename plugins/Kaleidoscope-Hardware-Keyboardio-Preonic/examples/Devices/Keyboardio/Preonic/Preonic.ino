bfda8e5ce2e2d2b654106eb295cc99bc54a15c56
Jesse Vincent <jesse@fsck.com>
2025-09-08 19:39:09 -0700

    A no-op documentation change to trigger a rebuild to pick up a Windows fix in the nRF52 Arduino core for the Preonic

--- plugins/Kaleidoscope-Hardware-Keyboardio-Preonic/examples/Devices/Keyboardio/Preonic/Preonic.ino
+++ plugins/Kaleidoscope-Hardware-Keyboardio-Preonic/examples/Devices/Keyboardio/Preonic/Preonic.ino
@@ -19,6 +19,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef BUILD_INFORMATION
 #define BUILD_INFORMATION "locally built on " __DATE__ " at " __TIME__
 #endif
